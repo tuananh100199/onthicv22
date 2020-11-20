61dfafd9a85a3ecd3f43286e31e45ee2ecc019a6
Nguyen The Vinh <vinh.nguyen24.la@gmail.com>
2020-11-20 17:11:56 +0700

    fix string

--- module/fwNews/sectionNewsList.jsx
+++ module/fwNews/sectionNewsList.jsx
@@ -14,7 +14,7 @@ class NewsListView extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      viewMode: (T.getCookie('viewMode') == '' ? 'list' : T.getCookie('viewMode')) //TODO: Cookie
+      viewMode: (T.getCookie('viewMode').slice(1, T.getCookie('viewMode').length - 1) == '' ? 'list' : T.getCookie('viewMode').slice(1, T.getCookie('viewMode').length - 1)) //TODO: Cookie
     }
   }
 
@@ -39,7 +39,7 @@ class NewsListView extends React.Component {
   }
 
   render() {
-    console.log(T.getCookie('viewMode'))
+    console.log(T.getCookie('viewMode').slice(1, T.getCookie('viewMode').length - 1))
     let userPage = this.props.news ? this.props.news.userPage : null,
       elements_grid = [],
       elements_list = [];
@@ -120,7 +120,6 @@ class NewsListView extends React.Component {
         <div className='row mb-5'>
           {(this.state.viewMode == 'list') ? elements_list : elements_grid}
         </div>
-        <div>{(this.state.viewMode == '"list"') ? "yes" : "no"}</div>
       </ section >
     );
   }
