07eb34e797542f7118840bb4963892c2d6f1cc65
Nguyen Quang Sang <9a2.nguyenquangsang@gmail.com>
2020-12-09 12:19:29 +0700

    Chỉnh sửa giao diện list videos

--- module/fwHome/sectionListVideo.jsx
+++ module/fwHome/sectionListVideo.jsx
@@ -1,21 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { getAllVideos} from './redux/reduxVideo.jsx';
-import { getListVideoItem } from './redux/reduxListVideo.jsx';
+import { getAllVideosByUser } from './redux/reduxVideo.jsx';
+import { getListVideoByUser } from './redux/reduxListVideo.jsx';
 
 class SectionListVideo extends React.Component {
-    state = {};
-
+    state = { item: {}, items: [] };
+    
     componentDidMount() {
         if (this.props.listVideoId) {
-            this.props.getListVideoItem(this.props.listVideoId, data => {
-                    if (data.error) {
-                       console.log('list các video trống')
-                    } 
-                    else if (data.item) {
-                        this.props.getAllVideos({ listVideoId : data.item._id }, (items) => {
-                            if (items) {
-                                this.setState({ item : data.item, items : items});
+            this.props.getListVideoByUser(this.props.listVideoId, data => {
+                if (data.error) {
+                    console.log('list các video trống')
+                } else if (data.item) {
+                    this.props.getAllVideosByUser({ listVideoId: data.item._id }, (items) => {
+                        if (items) {
+                            this.setState({ item: data.item, items }, () => {
                                 const done = () => {
                                     const elements = $('.popup-youtube, .popup-vimeo, .popup-gmaps');
                                     if (elements.length > 0) {
@@ -32,52 +31,43 @@ class SectionListVideo extends React.Component {
                                     }
                                 };
                                 done();
-                            }
-                        });
-                    } else {
-                        this.props.history.push('/user/component');
-                    }
-                
+                            });
+                        }
+                    });
+                }
             });
         }
     }
     
     render() {
-        let arr = this.state.items;
-        let firstItem = [];
-        if (arr) {
-            firstItem = arr.splice(0,1);
-        }
-        return this.state.items ? (
+        let { item, items } = this.state;
+        const firstItem = items && items.length ? items.splice(0 ,1)[0] : null;
+        return (
             <div>
-                <h3>Video clip</h3>
-                <div className='row' style={{ padding: '5px',  height: this.state.item.height + 20 + 'px'}}>
-                    <div className='col-md-9 p-0 m-0'>
-                        {firstItem.map((item, index) => (
-                            <div key={index} style={{height: '100%', padding: '5px'}}>
-                                <a href={item.link} className='button popup-youtube d-flex justify-content-center align-items-center'>
-                                    <img style={{ height: this.state.item.height + 'px' }} src={item.image} alt='videoImage' />
-                                </a>
-                            </div>
-                        ))}
-                    </div>
-                    <div className='col-md-3 m-0 p-0'>
-                        <div className='custom-scroll' style = {{ height: this.state.item.height + 'px', color: 'white', overflowY: 'auto', margin: '5px 0' }} >
-                            {arr.map((item, index) => (
-                                <div key={index}>
-                                    <a href={item.link} className='button popup-youtube d-flex justify-content-center align-items-center'>
-                                        <img height='85px' src= {item.image} alt='videoImage' />
+                <h3 className='text-primary'>Video clip</h3>
+                {firstItem ? (
+                    <div className='row' style={{ padding: '5px', height: item.height + 20 + 'px' }}>
+                        <div className='col-md-12 col-lg-9 p-0 m-0'>
+                            <a href={firstItem.link} className='button popup-youtube d-flex justify-content-center align-items-center'>
+                                <img style={{ height: item.height + 'px' }} src={firstItem.image} alt='videoImage'/>
+                            </a>
+                        </div>
+                        <div className='col-md-12 col-lg-3 m-0 p-0'>
+                            <div className='custom-scroll' style={{ height: item.height + 'px', color: 'white', overflowY: 'auto', margin: '5px 0' }}>
+                                {items.map((item, index) => (
+                                    <a key={index} href={item.link} className='button popup-youtube d-flex justify-content-center align-items-center'>
+                                        <img height='85px' src={item.image} alt='videoImage'/>
                                     </a>
-                                </div>
-                            ))}
+                                ))}
+                            </div>
                         </div>
                     </div>
-                </div>
+                ) : <p>Không có danh sách videos</p>}
             </div>
-        ) : '';
+        )
     }
 }
 
-const mapStateToProps = state => ({ video: state.video });
-const mapActionsToProps = { getAllVideos, getListVideoItem };
+const mapStateToProps = state => ({ });
+const mapActionsToProps = { getAllVideosByUser, getListVideoByUser };
 export default connect(mapStateToProps, mapActionsToProps)(SectionListVideo);
