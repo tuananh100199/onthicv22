f569acf2e9bf7bc9b4e1530eec2ae6c1c32294e3
vantam10a11 <vantam10a11@gmail.com>
2021-01-25 11:01:03 +0700

    add model & controller

---
+++ module/fwAddress/controller.js
@@ -0,0 +1,35 @@
+module.exports = app => {
+    app.get('/api/address/all', app.permission.check('component:read'), (req, res) => {
+        app.model.address.getAll((error, items) => {
+            res.send({ error, items })
+        })
+    });
+
+    app.get('/api/address/item/:addressId', app.permission.check('component:read'), (req, res) =>
+        app.model.address.get(req.params.addressId, (error, item) => res.send({ error, item })));
+
+    app.post('/api/address', app.permission.check('component:write'), (req, res) => {
+        app.model.address.create(req.body.newData, (error, item) => res.send({ error, item }))
+    });
+
+    app.put('/api/address', app.permission.check('component:write'), (req, res) => {
+        const $set = req.body.changes;
+        if ($set && $set.items && $set.items === 'empty') $set.items = [];
+        app.model.address.update(req.body._id, $set, (error, item) => res.send({ error, item }))
+    });
+
+    app.delete('/api/address', app.permission.check('component:write'), (req, res) => app.model.address.delete(req.body._id, error => res.send({ error })));
+
+    // Hook upload images ---------------------------------------------------------------------------------------------------------------------------s
+
+    app.createFolder(app.path.join(app.publicPath, '/img/address'));
+
+    const uploadAddress = (req, fields, files, params, done) => {
+        if (fields.userData && fields.userData[0].startsWith('address:') && files.AddressImage && files.AddressImage.length > 0) {
+            console.log('Hook: uploadVideo => video image upload');
+            app.uploadComponentImage(req, 'address', app.model.address.get, fields.userData[0].substring(8), files.AddressImage[0].path, done);
+        }
+    };
+    app.uploadHooks.add('uploadAddress', (req, fields, files, params, done) =>
+        app.permission.has(req, () => uploadAddress(req, fields, files, params, done), done, 'component:write'));
+};
---
+++ module/fwAddress/model.js
@@ -0,0 +1,55 @@
+
+module.export = (app) => {
+    const schema = app.db.Schema({
+        title: String,
+        address: String,
+        phoneNumber: String,
+        mobile: String,
+        email: String,
+        image: String,
+        priority: Number,
+        mapURL: String,
+    });
+    const model = app.db.model('Address', schema);
+
+    app.model.address = {
+        create: (data, done) => model.find({}).sort({ priority: -1 }).limit(1).exec((error, items) => {
+            data.priority = error || items == null || items.length === 0 ? 1 : items[0].priority + 1;
+            model.create(data, (error, item) => {
+                if (error) {
+                    done(error);
+                } else {
+                    item.image = '/img/address/' + item._id + '.jpg';
+                    const srcPath = app.path.join(app.publicPath, '/img/avatar.jpg'),
+                        destPath = app.path.join(app.publicPath, item.image);
+                    app.fs.copyFile(srcPath, destPath, error => {
+                        if (error) {
+                            done(error);
+                        } else {
+                            item.save(done);
+                        }
+                    });
+                }
+            });
+        }),
+
+        getAll: (condition, done) => done ? model.find(condition).sort({ priority: -1 }).exec(done) : model.find({}).sort({ title: -1 }).exec(condition),
+
+        get: (condition, done) => typeof condition == 'string' ? model.findById(condition).exec(done) : model.findOne(condition).exec(done),
+
+        update: (_id, $set, $unset, done) => done ?
+            model.findOneAndUpdate({ _id }, { $set, $unset }, { new: true }).exec(done) :
+            model.findOneAndUpdate({ _id }, { $set }, { new: true }).exec($unset),
+
+        delete: (_id, done) => model.findById(_id, (error, item) => {
+            if (error) {
+                done(error);
+            } else if (item == null) {
+                done('Invalid Id!');
+            } else {
+                app.deleteImage(item.image);
+                item.remove(done);
+            }
+        }),
+    };
+}
