19014c23bd1da9dbea58b37c6e0aef983fcdd287
letrongphu <letrongphu142@gmail.com>
2021-02-24 16:08:23 +0700

    subscribe

--- module/fwHome/sectionSubscribe.jsx
+++ module/fwHome/sectionSubscribe.jsx
@@ -1,62 +1,45 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { getStatisticByUser } from './redux/reduxStatistic.jsx';
 
 class SectionSubscribe extends React.Component {
+    background = React.createRef();
     state = {};
 
     componentDidMount() {
-        if (this.props.statisticId) {
-            this.props.getStatisticByUser(this.props.statisticId, statistic => this.setState({ statistic }));
-        }
+        $(this.background.current).parallax();
     }
 
-    // componentDidUpdate() {
-    //     setTimeout(() => {
-    //         $('.section-counter-class').waypoint(function (direction) {
-    //             if (direction === 'down' && !$(this.element).hasClass('ftco-animated')) {
-    //                 const comma_separator_number_step = $.animateNumber.numberStepFactories.separator(',')
-    //                 $('.number').each(function () {
-    //                     const $this = $(this);
-    //                     $this.animateNumber({ number: $this.data('number'), numberStep: comma_separator_number_step }, 7000);
-    //                 });
-    //             }
-    //         }, { offset: '95%' });
-    //         T.ftcoAnimate();
-    //     }, 250);
-    // }
+    componentWillUnmount = () => {
+        $(this.background.current).parallax('destroy');
+    }
 
     render() {
-
+        let subscribe = this.props && this.props.system && this.props.system.subscribe ? this.props.system.subscribe : '/img/subscribe.jpg';
         return (
-            <section className='ftco-section ftco-counter section-counter-class img' style={{ backgroundImage: 'url(\'' + '' + '\')' }}>
-                <div className='container-fluid'>
-                    <div className='row justify-content-center mb-5 pb-3'>
-                        <div className='col-md-7 text-center heading-section heading-section-white ftco-animate'>
-                            <h2 className='mb-4 text'>Subscribe to our newsletter</h2>
-                            {/* <span className='subheading text' dangerouslySetInnerHTML={{ __html: T.language.parse(this.state.statistic.description) }} /> */}
+            <div className="newsletter">
+                <div ref={this.background} className="parallax_background parallax-window" data-parallax="scroll" data-image-src={subscribe} data-speed="0.8"></div>
+                <div className="container">
+                    <div className="row">
+                        <div className="col text-center">
+                            <div className="newsletter_title">Subscribe to our newsletter</div>
                         </div>
                     </div>
-                    <div className='row justify-content-center'>
-                        <div className='col-md-10'>
-                            <div className='row justify-content-center'>
-                                <div className="col-lg-8 offset-lg-2">
-                                    <div className="newsletter_form_container">
-                                        <form action="#" id="newsleter_form" className="newsletter_form">
-                                            <input type="text" className="newsletter_input" placeholder="Your E-mail" required="required" />
-                                            <button className="newsletter_button">subscribe</button>
-                                        </form>
-                                    </div>
-                                </div>
+                    <div className="row newsletter_row">
+                        <div className="col-lg-8 offset-lg-2">
+                            <div className="newsletter_form_container">
+                                <form action="#" id="newsleter_form" className="newsletter_form">
+                                    <input type="email" className="newsletter_input" placeholder="Your E-mail" required="required" />
+                                    <button className="newsletter_button">subscribe</button>
+                                </form>
                             </div>
                         </div>
                     </div>
                 </div>
-            </section>
+            </div>
         );
     }
 }
 
-const mapStateToProps = state => ({ statistic: state.statistic });
-const mapActionsToProps = { getStatisticByUser };
+const mapStateToProps = state => ({ system: state.system });
+const mapActionsToProps = {};
 export default connect(mapStateToProps, mapActionsToProps)(SectionSubscribe);
