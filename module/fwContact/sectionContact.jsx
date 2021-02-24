39ce19cef7916c97d4e944cfd6c4ca43bf8b6d3b
Nguyen The Vinh <vinh.nguyen24.la@gmail.com>
2021-02-24 10:54:54 +0700

    fix img contact

--- module/fwContact/sectionContact.jsx
+++ module/fwContact/sectionContact.jsx
@@ -49,6 +49,10 @@ class SectionContact extends React.Component {
     render() {
         let { addressList, mobile, email, map, facebook, youtube, twitter, instagram, contact, subscribe, latitude, longitude } =
             this.props.system ? this.props.system : { addressList: JSON.stringify([]), map: '', facebook: '', youtube: '', twitter: '', instagram: '', contact: '/img/contact.jpg', subscribe: '/img/subscribe.jpg', latitude: 0, longitude: 0 };
+        facebook = facebook ? <li><a href={facebook} target='_blank'><i className='fa fa-facebook' aria-hidden='true' /></a></li> : '';
+        youtube = youtube ? <li><a href={youtube} target='_blank'><i className='fa fa-youtube' aria-hidden='true' /></a></li> : '';
+        twitter = twitter ? <li><a href={twitter} target='_blank'><i className='fa fa-twitter' aria-hidden='true' /></a></li> : '';
+        instagram = instagram ? <li><a href={instagram} target='_blank'><i className='fa fa-instagram' aria-hidden='true' /></a></li> : '';
         const mapUrl = 'https://www.google.com/maps/@' + latitude + ',' + longitude + ',16z';
 
         const styles = {
@@ -69,7 +73,7 @@ class SectionContact extends React.Component {
             <div className='container-fluid'>
                 <div className='row'>
                     <div className='col-12 px-0'>
-                        <div className="owl-theme">
+                        <div className='owl-theme'>
                             <div className='slider-content'
                                 style={{
                                     height: '500px',
@@ -89,37 +93,37 @@ class SectionContact extends React.Component {
             <div className='contact'>
                 <div className='container'>
                     <div className='row'>
-                        <div className="col-lg-6">
-                            <div className="contact_form_container">
-                                <div className="contact_form_title">Make an Appointment</div>
-                                <form className="contact_form" id="contact_form" onSubmit={this.sendMessage}>
-                                    <div className="d-flex flex-row align-items-start justify-content-between flex-wrap">
-                                        <input type="text" className="contact_input" ref={this.name} placeholder='Tên' />
-                                        <input type="email" className="contact_input" ref={this.email} placeholder='Email' />
-                                        <input type="text" className="contact_input_subject" ref={this.subject} placeholder='Chủ đề' />
-                                        <textarea name='message' className='contact_input_subject' ref={this.message} cols='30' rows='10' placeholder='Nội dung' />
+                        <div className='col-lg-6'>
+                            <div className='contact_form_container'>
+                                <div className='contact_form_title'>Gửi Tin Nhắn</div>
+                                <form className='contact_form' id='contact_form' onSubmit={this.sendMessage}>
+                                    <div className='d-flex flex-row align-items-start justify-content-between flex-wrap'>
+                                        <input type='text' className='contact_input' ref={this.name} placeholder='Tên' />
+                                        <input type='email' className='contact_input' ref={this.email} placeholder='Email' />
+                                        <input type='text' className='contact_input w-100' ref={this.subject} placeholder='Chủ đề' />
+                                        <textarea name='message' className='contact_input w-100' ref={this.message} cols='30' rows='10' placeholder='Nội dung' />
                                     </div>
-                                    <button className="button button_1 contact_button trans_200">make an appointment</button>
+                                    <button className='button button_1 contact_button trans_200'>gửi tin nhắn</button>
                                 </form>
                             </div>
                         </div>
-                        <div className="col-lg-5 offset-lg-1 contact_col">
-                            <div className="contact_content">
-                                <div className="contact_content_title">Thông tin liên lạc</div>
+                        <div className='col-lg-5 offset-lg-1 contact_col'>
+                            <div className='contact_content'>
+                                <div className='contact_content_title'>Thông tin liên lạc</div>
                                 {this.props.address && this.props.address.list && this.props.address.list.length > 0 ?
                                     this.props.address.list.map((item, index) => (
                                         [
-                                            <div className="contact_info" key={index}>
+                                            <div className='contact_info' key={index}>
                                                 <ul>
-                                                    <li className="d-flex flex-row align-items-start justify-content-start">
+                                                    <li className='d-flex flex-row align-items-start justify-content-start'>
                                                         <div>{item.title}:</div>
                                                         <div>{item.address}</div>
                                                     </li>
-                                                    <li className="d-flex flex-row align-items-start justify-content-start">
+                                                    <li className='d-flex flex-row align-items-start justify-content-start'>
                                                         <div>Điện thoại:</div>
                                                         <div><a href={'tel:' + item.phoneNumber}>{item.phoneNumber}</a></div>
                                                     </li>
-                                                    <li className="d-flex flex-row align-items-start justify-content-start">
+                                                    <li className='d-flex flex-row align-items-start justify-content-start'>
                                                         <div>E-mail</div>
                                                         <div><a href={'mailto:' + item.email}>{item.email}</a></div>
                                                     </li>
@@ -128,14 +132,12 @@ class SectionContact extends React.Component {
                                         ]
                                     )) : <p>Chưa cập nhật địa chỉ</p>}
 
-                                <div className="contact_social">
-                                    <ul className="d-flex flex-row align-items-center justify-content-start">
-                                        <li><a href="#"><i className="fa fa-pinterest" aria-hidden="true"></i></a></li>
-                                        <li><a href="#"><i className="fa fa-facebook" aria-hidden="true"></i></a></li>
-                                        <li><a href="#"><i className="fa fa-twitter" aria-hidden="true"></i></a></li>
-                                        <li><a href="#"><i className="fa fa-dribbble" aria-hidden="true"></i></a></li>
-                                        <li><a href="#"><i className="fa fa-behance" aria-hidden="true"></i></a></li>
-                                        <li><a href="#"><i className="fa fa-linkedin" aria-hidden="true"></i></a></li>
+                                <div className='contact_social'>
+                                    <ul className='d-flex flex-row align-items-center justify-content-start'>
+                                        {twitter}
+                                        {facebook}
+                                        {youtube}
+                                        {instagram}
                                     </ul>
                                 </div>
                             </div>
@@ -143,27 +145,6 @@ class SectionContact extends React.Component {
                     </div>
                 </div>
             </div >,
-            <div className="newsletter">
-                <div className="parallax_background parallax-window" data-parallax="scroll" data-image-src="/img/contact.jpg" data-speed="0.8"></div>
-                <div className="container">
-                    <div className="row">
-                        <div className="col text-center">
-                            <div className="newsletter_title">Subscribe to our newsletter</div>
-                        </div>
-                    </div>
-                    <div className="row newsletter_row">
-                        <div className="col-lg-8 offset-lg-2">
-                            <div className="newsletter_form_container">
-                                <form action="#" id="newsleter_form" className="newsletter_form">
-                                    <input type="email" className="newsletter_input" placeholder="Your E-mail" required="required" />
-                                    <button className="newsletter_button">subscribe</button>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
         ];
     }
 }
