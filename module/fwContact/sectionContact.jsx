500876defc532964ba531f0061005d65c4c4bfaa
Nguyen The Vinh <vinh.nguyen24.la@gmail.com>
2021-02-24 11:08:26 +0700

    fix img contact

--- module/fwContact/sectionContact.jsx
+++ module/fwContact/sectionContact.jsx
@@ -70,20 +70,17 @@ class SectionContact extends React.Component {
         }
 
         return [
-            <div className='container-fluid'>
-                <div className='row'>
-                    <div className='col-12 px-0'>
-                        <div className='owl-theme'>
-                            <div className='slider-content'
-                                style={{
-                                    height: '500px',
-                                    background: `url('${contact}')`,
-                                    backgroundRepeat: 'repeat',
-                                    backgroundPosition: 'center',
-                                    backgroundSize: 'cover'
-                                }}
-                            >
-
+            <div class="home d-flex flex-column align-items-start justify-content-end">
+                <div class="parallax_background parallax-window" data-parallax="scroll" data-image-src={contact} data-speed="0.8"></div>
+                <div class="home_overlay"><img src="images/home_overlay.png" alt="" /></div>
+                <div class="home_container">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col">
+                                <div class="home_content">
+                                    <div class="home_title">Liên hệ</div>
+                                    <div class="home_text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div>
+                                </div>
                             </div>
                         </div>
                     </div>
