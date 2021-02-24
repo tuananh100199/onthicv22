4586f0bb9ee45cf86ba462223fd76bccd9930b66
letrongphu <letrongphu142@gmail.com>
2021-02-24 11:50:27 +0700

    tmp 2

--- module/fwHome/sectionSubscribe.jsx
+++ module/fwHome/sectionSubscribe.jsx
@@ -1,58 +1,62 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { getStatisticByUser } from './redux/reduxStatistic.jsx';
 
 class SectionSubscribe extends React.Component {
     state = {};
 
-    componentDidMount() {}
-
-    componentDidUpdate() {
-        setTimeout(() => {
-            $('.section-counter-class').waypoint(function (direction) {
-                if (direction === 'down' && !$(this.element).hasClass('ftco-animated')) {
-                    const comma_separator_number_step = $.animateNumber.numberStepFactories.separator(',')
-                    $('.number').each(function () {
-                        const $this = $(this);
-                        $this.animateNumber({ number: $this.data('number'), numberStep: comma_separator_number_step }, 7000);
-                    });
-                }
-            }, { offset: '95%' });
-            T.ftcoAnimate();
-        }, 250);
+    componentDidMount() {
+        if (this.props.statisticId) {
+            this.props.getStatisticByUser(this.props.statisticId, statistic => this.setState({ statistic }));
+        }
     }
+
+    // componentDidUpdate() {
+    //     setTimeout(() => {
+    //         $('.section-counter-class').waypoint(function (direction) {
+    //             if (direction === 'down' && !$(this.element).hasClass('ftco-animated')) {
+    //                 const comma_separator_number_step = $.animateNumber.numberStepFactories.separator(',')
+    //                 $('.number').each(function () {
+    //                     const $this = $(this);
+    //                     $this.animateNumber({ number: $this.data('number'), numberStep: comma_separator_number_step }, 7000);
+    //                 });
+    //             }
+    //         }, { offset: '95%' });
+    //         T.ftcoAnimate();
+    //     }, 250);
+    // }
 
     render() {
 
         return (
-            <section className='ftco-section ftco-counter section-counter-class img' style={{ backgroundImage: 'url(\'' + this.state.statistic.background + '\')' }}>
+            <section className='ftco-section ftco-counter section-counter-class img' style={{ backgroundImage: 'url(\'' + '' + '\')' }}>
                 <div className='container-fluid'>
                     <div className='row justify-content-center mb-5 pb-3'>
                         <div className='col-md-7 text-center heading-section heading-section-white ftco-animate'>
-                            <h2 className='mb-4 text'>{T.language.parse(this.state.statistic.title)}</h2>
-                            <span className='subheading text' dangerouslySetInnerHTML={{ __html: T.language.parse(this.state.statistic.description) }} />
+                            <h2 className='mb-4 text'>Subscribe to our newsletter</h2>
+                            {/* <span className='subheading text' dangerouslySetInnerHTML={{ __html: T.language.parse(this.state.statistic.description) }} /> */}
                         </div>
                     </div>
                     <div className='row justify-content-center'>
                         <div className='col-md-10'>
                             <div className='row justify-content-center'>
-                                {this.state.statistic.items.map((item, index) => (
-                                    <div key={index} className={itemClassName}>
-                                        <div className='block-18 text-center'>
-                                            <div className='text'>
-                                                <strong className='number' data-number={item.number}>{item.number}</strong>
-                                                <span>{T.language.parse(item.title)}</span>
-                                            </div>
-                                        </div>
+                                <div className="col-lg-8 offset-lg-2">
+                                    <div className="newsletter_form_container">
+                                        <form action="#" id="newsleter_form" className="newsletter_form">
+                                            <input type="text" className="newsletter_input" placeholder="Your E-mail" required="required" />
+                                            <button className="newsletter_button">subscribe</button>
+                                        </form>
                                     </div>
-                                ))}
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </section>);
+            </section>
+        );
     }
 }
 
 const mapStateToProps = state => ({ statistic: state.statistic });
-const mapActionsToProps = {  };
+const mapActionsToProps = { getStatisticByUser };
 export default connect(mapStateToProps, mapActionsToProps)(SectionSubscribe);
