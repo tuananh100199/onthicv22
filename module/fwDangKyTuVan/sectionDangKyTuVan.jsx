2bd702f36fc2c9336c990e3144af751c9b95f6c6
Trần Anh <trantuananh1001199999@gmail.com>
2021-03-02 10:46:14 +0700

    update fwDangKyTuVan

--- module/fwDangKyTuVan/sectionDangKyTuVan.jsx
+++ module/fwDangKyTuVan/sectionDangKyTuVan.jsx
@@ -57,64 +57,40 @@ class SectionDangKyTuVan extends React.Component {
     }
 
     render() {
-        console.log(this.props.system.user);
+        let { title, description } = this.props.system ?
+        this.props.system : {title: '', description: '' };
         return [
             <div  key={1} className="intro">
                 <div className="container">
                     <div className="row">
-                    <div className="col-lg-6 intro_col">
-                    {this.props.DangKyTuVan && this.props.DangKyTuVan.list && this.props.DangKyTuVan.list.length > 0 ?
-                     this.props.DangKyTuVan.list.map((item, index) => (
-                    <div className="intro_content"  key={index}>
-                        <div className="section_title_container">
-                            <div className="section_title"><h2>{item.title}:&nbsp;</h2></div>
-                        </div>
-                        <div className="intro_text">
-                            <p>{item.description}</p>
-                        </div>
-                        <div className="milestones">
-                            <div className="row milestones_row">
-
-                                <div className="col-md-4 milestone_col">
-                                    <div className="milestone">
-                                        <div className="milestone_counter" data-end-value={5000} data-sign-before="+">0</div>
-                                        <div className="milestone_text">Satisfied Patients</div>
-                                    </div>
-                                </div>
-                                <div className="col-md-4 milestone_col">
-                                    <div className="milestone">
-                                        <div className="milestone_counter" data-end-value={352}>0</div>
-                                        <div className="milestone_text">Face Liftings</div>
-                                    </div>
-                                </div>
-                                <div className="col-md-4 milestone_col">
-                                    <div className="milestone">
-                                        <div className="milestone_counter" data-end-value={718}>0</div>
-                                        <div className="milestone_text">Injectibles</div>
-                                    </div>
-                                </div>
+                        <div className="col-lg-6 intro_col">
+                            <div className="intro_content" >
+                            <div className="section_title_container">
+                                <div className="section_title"><h2>{title}&nbsp;</h2></div>
+                            </div>
+                            <div className="intro_text">
+                                <p>{description}</p>
                             </div>
                         </div>
-                    </div>)): <p>Chưa cập nhật địa chỉ</p>}
-                    </div>
-                    <div className="col-lg-6 intro_col">
-                        <div className="intro_form_container">
-                            <div className="intro_form_title">Đăng ký tư vấn</div>
-                            <form action="#" className="intro_form" id="intro_form" onSubmit={this.sendMessage}>
-                                <div className="d-flex flex-row align-items-start justify-content-between flex-wrap">
-                                    <input type="text" className="intro_input" placeholder="Tên"  ref={this.name} required="required" />
-                                    <input type="tel" className="intro_input" placeholder="Số điện thoại"  ref={this.phone} required="required" />
-                                    <input type='text' className='contact_input w-100' ref={this.email} placeholder='Email' required="required" />
-                                    <input type='text' className='contact_input w-100' ref={this.subject} placeholder='Chủ đề' />
-                                    <textarea name='message' className='contact_input w-100' ref={this.message} cols='30' rows='10' placeholder='Nội dung' />
-                                </div>
-                                <button className="button button_1 intro_button trans_200">gửi tin nhắn</button>
-                            </form>
+                            </div>
+                        <div className="col-lg-6 intro_col">
+                            <div className="intro_form_container">
+                                <div className="intro_form_title">Đăng ký tư vấn</div>
+                                <form action="#" className="intro_form" id="intro_form" onSubmit={this.sendMessage}>
+                                    <div className="d-flex flex-row align-items-start justify-content-between flex-wrap">
+                                        <input type="text" className="intro_input" placeholder="Tên"  ref={this.name} required="required" />
+                                        <input type="tel" className="intro_input" placeholder="Số điện thoại"  ref={this.phone} required="required" />
+                                        <input type='text' className='contact_input w-100' ref={this.email} placeholder='Email' required="required" />
+                                        <input type='text' className='contact_input w-100' ref={this.subject} placeholder='Chủ đề' />
+                                        <textarea name='message' className='contact_input w-100' ref={this.message} cols='30' rows='10' placeholder='Nội dung' />
+                                    </div>
+                                    <button className="button button_1 intro_button trans_200">gửi tin nhắn</button>
+                                </form>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
         ];
     }
 }
