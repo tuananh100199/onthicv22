7035cfbd29a2d9a4ebc075fb9f6ca33c9ba90fd8
hithanhtung <hithanhtung@gmail.com>
2021-03-18 16:08:27 +0700

    drive question

---
+++ modules/mdDaoTao/fwDriveQuestion/adminDriveQuestionPage.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { } from './reduxDriveQuestion';
+import { } from './reduxDriveQuestionCategory';
+import { Link } from 'react-router-dom';
+import { AdminPage, AdminModal, FormTextBox } from 'view/component/AdminPage';
+
+class QuestionModal extends AdminModal {
+    // TODO: Tuấn Anh
+}
+
+class AdminPage extends AdminPage {
+    // TODO: Tuấn Anh
+}
+
+const mapStateToProps = state => ({ system: state.system, driveQuestion: state.driveQuestion, driveQuestionCategory: state.driveQuestionCategory });
+const mapActionsToProps = {};
+export default connect(mapStateToProps, mapActionsToProps)(AdminPage);
