3a3c6d1c8577faa7889635ca060ee4d959f837c3
Trần Anh <trantuananh1001199999@gmail.com>
2021-03-18 17:29:41 +0700

    update drive question category

--- modules/mdDaoTao/fwDriveQuestion/adminDriveQuestionCategoryPage.jsx
+++ modules/mdDaoTao/fwDriveQuestion/adminDriveQuestionCategoryPage.jsx
@@ -1,17 +1,120 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { } from './reduxDriveQuestionCategory';
+import { getAllDriveQuestionCategory, createDriveQuestionCategory, deleteDriveQuestionCategory, updateDriveQuestionCategory,  getDriveQuestionCategoryItem } from './reduxDriveQuestionCategory';
+
 import { Link } from 'react-router-dom';
 import { AdminPage, AdminModal, FormTextBox } from 'view/component/AdminPage';
 
 class CategoryModal extends AdminModal {
-    // TODO: Tuấn Anh
+    componentDidMount() {
+        $(document).ready(() => this.onShown(() => this.itemTitle.focus()));
+    }
+
+    onShow = () => this.itemTitle.value('');
+
+    onSubmit = () => {
+        const newData = { title: this.itemTitle.value() };
+        if (newData.title == '') {
+            T.notify('Tên loại câu hỏi thi bị trống!', 'danger');
+            this.itemTitle.focus();
+        } else {
+            this.props.createDriveQuestionCategory(newData, data => {
+                if (data.item) {
+                    this.hide();
+                    this.props.history.push('/user/drive-question-category/edit/' + data.item._id);
+                }
+            });
+        }
+    }
+
+    render = () => this.renderModal({
+        title: 'Loại câu hỏi thi mới',
+        body: <FormTextBox ref={e => this.itemTitle = e} label='Tên loại câu hỏi thi' />
+    });
 }
 
-class AdminPage extends AdminPage {
-    // TODO: Tuấn Anh
+class DriveQuestionCategoryPage extends AdminPage {
+    modal = React.createRef();
+
+    componentDidMount() {
+        T.ready();
+        this.props.getAllDriveQuestionCategory();
+        T.onSearch = (searchText) => this.props.getAllDriveQuestionCategory(searchText);
+    }
+
+    create = (e) => {
+        this.modal.current.show();
+        e.preventDefault();
+    }
+
+    edit = (e, _id) => {
+        this.props.getDriveQuestionCategoryItem(_id, item => this.modal.current.show(item));
+        e.preventDefault();
+    }
+
+    delete = (e, item) => {
+        T.confirm('Xóa loại câu hỏi thi', 'Bạn có chắc bạn muốn xóa loại câu hỏi thi này?', true, isConfirm => isConfirm && this.props.deleteDriveQuestionCategory(item._id));
+        e.preventDefault();
+    }
+
+    render() {
+        const permission = this.getUserPermission('driveQuestionCategory');
+        let table = 'Không có loại câu hỏi thi!';
+        if (this.props.driveQuestionCategory && this.props.driveQuestionCategory.list && this.props.driveQuestionCategory.list.length > 0) {
+            table = (
+                <table className='table table-hover table-bordered'>
+                    <thead>
+                        <tr>
+                            <th style={{ width: 'auto', textAlign: 'center' }}>#</th>
+                            <th style={{ width: '100%' }}>Tên loại câu hỏi thi</th>
+                            <th style={{ width: 'auto', textAlign: 'center' }} nowrap='true'>Kích hoạt</th>
+                            {permission.write || permission.delete ? <th style={{ width: 'auto', textAlign: 'center' }} nowrap='true'>Thao tác</th> : null}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.props.driveQuestionCategory.list.map((item, index) => (
+                            <tr key={index}>
+                                <td style={{ textAlign: 'right' }}>{index + 1}</td>
+                                <td><Link to={'/user/drive-question-category/edit/' + item._id}>{item.title}</Link></td>
+                                <td className='toggle' style={{ textAlign: 'center' }} >
+                                    <label>
+                                        <input type='checkbox' checked={item.isOutside}
+                                            onChange={() => permission.write && this.props.updateDriveQuestionCategory(item._id, { isOutside: item.isOutside ? 0 : 1 }, () => T.notify('Cập nhật loại câu hỏi thi thành công!', 'success'))} />
+                                        <span className='button-indecator' />
+                                    </label>
+                                </td>
+                                {permission.write || permission.delete ? <td>
+                                    <div className='btn-group'>
+                                        {permission.delete || permission.write ?
+                                            <a className='btn btn-primary' href='#' onClick={e => this.edit(e, item._id)}>
+                                                <i className='fa fa-lg fa-edit' />
+                                            </a> : null}
+                                        {permission.delete ?
+                                            <a className='btn btn-danger' href='#' onClick={e => this.delete(e, item)}>
+                                                <i className='fa fa-lg fa-trash' />
+                                            </a> : null}
+                                    </div>
+                                </td> : null}
+                            </tr>))}
+                    </tbody>
+                </table>);
+        }
+
+        const renderData = {
+            icon: 'fa fa-university',
+            title: 'Loại câu hỏi thi',
+            breadcrumb: ['Loại câu hỏi thi'],
+            content: <>
+                <div className='tile'>{table}</div>
+                <CategoryModal ref={this.modal} createDriveQuestionCategory={this.props.createDriveQuestionCategory} history={this.props.history} />
+            </>,
+        };
+        if (permission.write) renderData.onCreate = this.create;
+        return this.renderListPage(renderData);
+    }
 }
+
 
 const mapStateToProps = state => ({ system: state.system, driveQuestionCategory: state.driveQuestionCategory });
-const mapActionsToProps = {};
-export default connect(mapStateToProps, mapActionsToProps)(AdminPage);
+const mapActionsToProps = {getAllDriveQuestionCategory, createDriveQuestionCategory, deleteDriveQuestionCategory, updateDriveQuestionCategory, getDriveQuestionCategoryItem};
+export default connect(mapStateToProps, mapActionsToProps)(DriveQuestionCategoryPage);
--- modules/mdDaoTao/fwDriveQuestion/controller/driveQuestionCategory.js
+++ modules/mdDaoTao/fwDriveQuestion/controller/driveQuestionCategory.js
@@ -1,3 +1,56 @@
 module.exports = app => {
-    //TODO: Tuấn Anh
+    const menu = {
+        parentMenu: app.parentMenu.trainning,
+        menus: {
+            4050: { title: 'Danh mục câu hỏi thi', link: '/user/drive-question-category', icon: 'fa-envelope-o', backgroundColor: '#00897b' },
+        },
+    };
+    app.permission.add(
+        { name: 'driveQuestionCategory:read', menu },
+        { name: 'driveQuestionCategory:write' },
+        { name: 'driveQuestionCategory:delete' }
+    );
+
+    app.get('/user/drive-question-category', app.permission.check('driveQuestionCategory:read'), app.templates.admin);
+    app.get('/user/drive-question-category/edit/:id', app.permission.check('driveQuestionCategory:write'), app.templates.admin);
+
+    // APIs -----------------------------------------------------------------------------------------------------------
+    app.get('/api/drive-question-category/all', app.permission.check('driveQuestionCategory:read'), (req, res) => {
+        const condition = {}, searchText = req.query.searchText;
+        if (searchText) {
+            condition.title = new RegExp(searchText, 'i');
+        }
+        app.model.driveQuestionCategory.getAll(condition, (error, items) => res.send({ error, items }));
+    });
+
+    app.get('/api/drive-question-category/item/:_id', app.permission.check('driveQuestionCategory:read'), (req, res) =>
+        app.model.driveQuestionCategory.get(req.params._id, (error, item) => res.send({ error, item })));
+
+    app.post('/api/drive-question-category', app.permission.check('driveQuestionCategory:write'), (req, res) => {
+        app.model.driveQuestionCategory.create(req.body.newData, (error, item) => res.send({ error, item }));
+    });
+
+    app.put('/api/drive-question-category', app.permission.check('driveQuestionCategory:write'), (req, res) => {
+        app.model.driveQuestionCategory.update(req.body._id, req.body.changes, (error, item) => res.send({ error, item }));
+    });
+
+    app.delete('/api/drive-question-category', app.permission.check('driveQuestionCategory:write'), (req, res) => {
+        app.model.driveQuestionCategory.delete(req.body._id, error => res.send({ error }));
+    });
+
+    app.get('/home/drive-question-category/all', (req, res) => {
+        app.model.driveQuestionCategory.getAll((error, items) => res.send({ error, items }));
+    });
+
+    // Hook upload images ---------------------------------------------------------------------------------------------
+    app.createFolder(app.path.join(app.publicPath, '/img/drive-question-category'));
+
+    const uploadDriveQuestionCategory = (req, fields, files, params, done) => {
+        if (fields.userData && fields.userData[0].startsWith('driveQuestionCategory:') && files.DriveQuestionCategoryImage && files.DriveQuestionCategoryImage.length > 0) {
+            console.log('Hook: uploadDriveQuestionCategory => drive question category image upload');
+            app.uploadComponentImage(req, 'driveQuestionCategory', app.model.driveQuestionCategory.get, fields.userData[0].substring(9), files.DriveQuestionCategoryImage[0].path, done);
+        }
+    };
+    app.uploadHooks.add('uploadDriveQuestionCategory', (req, fields, files, params, done) =>
+        app.permission.has(req, () => uploadDriveQuestionCategory(req, fields, files, params, done), done, 'driveQuestionCategory:write'));
 };
