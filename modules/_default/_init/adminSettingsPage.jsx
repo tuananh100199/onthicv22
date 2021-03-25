b88c7661e0ac39042f16e38ff070b0501f3ae9fb
Trần Anh <trantuananh1001199999@gmail.com>
2021-03-25 11:21:50 +0700

    chuyển trang adminSettingPage

--- modules/_default/_init/adminSettingsPage.jsx
+++ modules/_default/_init/adminSettingsPage.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { saveSystemState } from './reduxSystem';
-import ImageBox from 'view/component/ImageBox';
-import { AdminPage, FormTextBox } from 'view/component/AdminPage';
-
-
+import { AdminPage, FormTextBox, FormImageBox } from 'view/component/AdminPage';
 class SettingsPage extends AdminPage {
     state = {};
     componentDidMount() {
         T.ready();
-        let { address, email, mobile, fax, facebook, youtube, twitter, instagram } = this.props.system ?
-        this.props.system : { address: '', email: '', mobile: '', fax: '', facebook: '', youtube: '', twitter: '', instagram: '', logo: '', footer: '/img/footer.jpg', contact: '/img/contact.jpg', subscribe: '/img/subscribe.jpg' };
+        let { address, email, mobile, fax, facebook, youtube, twitter, instagram, logo, footer, contact, subscribe } = this.props.system ?
+        this.props.system : { address: '', email: '', mobile: '', fax: '', facebook: '', youtube: '', twitter: '', instagram: '', logo: '/img/logo.jpg', footer: '/img/footer.jpg', contact: '/img/contact.jpg', subscribe: '/img/subscribe.jpg' };
         this.systemAddress.value(address);
         this.systemEmail.value(email);
         this.systemMobile.value(mobile);
@@ -19,6 +16,11 @@ class SettingsPage extends AdminPage {
         this.systemYoutube.value(youtube);
         this.systemTwitter.value(twitter);
         this.systemInstagram.value(instagram);
+        this.systemLogo.setData('logo', logo);
+        this.systemFooter.setData('footer', footer);
+        this.systemContact.setData('contact', contact);
+        this.systemSubscribe.setData('subscribe', subscribe);
+        this.setState({ logo, footer, contact, subscribe });
     }
 
     saveCommonInfo = () => {
@@ -54,27 +56,24 @@ class SettingsPage extends AdminPage {
     }
 
     render() {
-        let readOnly = false;
-        let { logo, footer, contact, subscribe } = this.props.system ?
-        this.props.system : { logo: '/img/logo.jpg', footer: '/img/footer.jpg', contact: '/img/contact.jpg', subscribe: '/img/subscribe.jpg' };
-        return (
-            <main className='app-content'>
-                <div className='app-title'>
-                    <h1><i className='fa fa-cog' /> Cấu hình</h1>
-                </div>
-                <div className='row'>
+        return this.renderPage({
+            icon: 'fa fa-cog',
+            title: 'Cài đặt hệ thống',
+            breadcrumb: ['Thông tin hệ thống'],
+            content: <>
+               <div className='row'>
                     <div className='col-md-6'>
                         <div className='tile'>
                             <h3 className='tile-title'>Thông tin chung</h3>
                             <div className='tile-body'>
-                                <FormTextBox ref={e => this.systemAddress = e} label='Địa chỉ' readOnly={readOnly} defaultValue='' />
-                                <FormTextBox ref={e => this.systemEmail = e} label='Email' readOnly={readOnly} defaultValue='' />
-                                <FormTextBox ref={e => this.systemMobile = e} label='Số điện thoại' readOnly={readOnly} defaultValue='' />
-                                <FormTextBox ref={e => this.systemFax = e} label='Fax' readOnly={readOnly} defaultValue='' />
-                                <FormTextBox ref={e => this.systemFacebook = e} label='Facebook' readOnly={readOnly} defaultValue='' />
-                                <FormTextBox ref={e => this.systemYoutube = e} label='You tube' readOnly={readOnly} defaultValue='' />
-                                <FormTextBox ref={e => this.systemTwitter = e} label='Twitter' readOnly={readOnly} defaultValue='' />
-                                <FormTextBox ref={e => this.systemInstagram = e} label='Instagram' readOnly={readOnly} defaultValue='' />
+                                <FormTextBox ref={e => this.systemAddress = e} label='Địa chỉ' defaultValue='' />
+                                <FormTextBox ref={e => this.systemEmail = e} label='Email' defaultValue='' />
+                                <FormTextBox ref={e => this.systemMobile = e} label='Số điện thoại' defaultValue='' />
+                                <FormTextBox ref={e => this.systemFax = e} label='Fax' defaultValue='' />
+                                <FormTextBox ref={e => this.systemFacebook = e} label='Facebook' defaultValue='' />
+                                <FormTextBox ref={e => this.systemYoutube = e} label='You tube' defaultValue='' />
+                                <FormTextBox ref={e => this.systemTwitter = e} label='Twitter' defaultValue='' />
+                                <FormTextBox ref={e => this.systemInstagram = e} label='Instagram' defaultValue='' />
                             </div>
                             <div className='tile-footer' style={{ textAlign: 'right' }}>
                                 <button className='btn btn-primary' type='button' onClick={this.saveCommonInfo}>
@@ -88,8 +87,8 @@ class SettingsPage extends AdminPage {
                         <div className='tile'>
                             <h3 className='tile-title'>Thay đổi mật khẩu</h3>
                             <div className='tile-body'>
-                                <FormTextBox ref={e => this.emailPassword1 = e} label='Mật khẩu mới' readOnly={readOnly} defaultValue='' autoComplete='new-password'/>
-                                <FormTextBox ref={e => this.emailPassword2 = e} label='Nhập lại mật khẩu mới' readOnly={readOnly} defaultValue='' autoComplete='new-password'/>
+                                <FormTextBox ref={e => this.emailPassword1 = e} label='Mật khẩu mới' defaultValue='' autoComplete='new-password'/>
+                                <FormTextBox ref={e => this.emailPassword2 = e} label='Nhập lại mật khẩu mới' defaultValue='' autoComplete='new-password'/>
                             </div>
                             <div className='tile-footer'>
                                 <div className='row'>
@@ -106,32 +105,17 @@ class SettingsPage extends AdminPage {
                             <h3 className='tile-title'>Hình ảnh</h3>
                             <div className='tile-body'>
                                 <div className='tile-body'>
-                                    <div className='form-group'>
-                                        <label className='control-label'>Logo công ty</label>
-                                        <ImageBox postUrl='/user/upload' uploadType='SettingImage' userData='logo' image={logo} />
-                                    </div>
-
-                                    <div className='form-group'>
-                                        <label className='control-label'>Hình nền cuối trang web</label>
-                                        <ImageBox postUrl='/user/upload' uploadType='SettingImage' userData='footer' image={footer} />
-                                    </div>
-
-                                    <div className='form-group'>
-                                        <label className='control-label'>Hình nền phần Liên hệ</label>
-                                        <ImageBox postUrl='/user/upload' uploadType='SettingImage' userData='contact' image={contact} />
-                                    </div>
-
-                                    <div className='form-group'>
-                                        <label className='control-label'>Hình nền phần Đăng ký nhận tin</label>
-                                        <ImageBox postUrl='/user/upload' uploadType='SettingImage' userData='subscribe' image={subscribe} />
-                                    </div>
+                                    <FormImageBox ref={e => this.systemLogo = e}  label='Logo công ty' uploadType='SettingImage' image={this.state.logo} />
+                                    <FormImageBox ref={e => this.systemFooter = e}  label='Hình nền cuối trang web' uploadType='SettingImage' image={this.state.footer} />
+                                    <FormImageBox ref={e => this.systemContact = e}  label='Hình nền phần liên hệ' uploadType='SettingImage' image={this.state.contact} />
+                                    <FormImageBox ref={e => this.systemSubscribe = e}  label='Hình nền phần đăng ký nhận tin' uploadType='SettingImage' image={this.state.subscribe} />
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </main>
-        );
+            </>,
+        });
     }
 }
 
