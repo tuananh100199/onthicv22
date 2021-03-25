12f6e574d9f2da6d8a5452ebd7982b1589a9a755
Nguyen The Vinh <vinh.nguyen24.la@gmail.com>
2021-03-25 21:18:23 +0700

    text-center

--- modules/_default/fwHome/sectionStatistic.jsx
+++ modules/_default/fwHome/sectionStatistic.jsx
@@ -15,29 +15,24 @@ class SectionStatistic extends React.Component {
         const itemLength = this.state.statistic ? this.state.statistic.items.length : 0,
             itemClassName = 'col-md-' + Math.round(12 / itemLength) + ' d-flex justify-content-center counter-wrap ftco-animate';
         return itemLength > 0 ? (
-            <section className='ftco-section ftco-counter section-counter-class img' style={{ backgroundImage: 'url(\'' + this.state.statistic.background + '\')' }}>
-                <div className='container-fluid'>
-                    <div className='justify-content-center'>
-                        <div className='text-center heading-section heading-section-white ftco-animate'>
-                            <h2 className='text'>{this.state.statistic.title}</h2>
-                            <span className='subheading text' dangerouslySetInnerHTML={{ __html: this.state.statistic.description }} />
-                        </div>
-                    </div>
-                    <div className='row justify-content-center'>
-                        <div className='col-md-10'>
-                            <div className='row justify-content-center'>
-                                {this.state.statistic.items.map((item, index) => (
-                                    <div key={index} className={itemClassName} >
-                                        <div className="milestone">
-                                            <div className="milestone_counter">{item.number}</div>
-                                            <div className="milestone_text">{item.title}</div>
-                                        </div>
-                                    </div>
-                                ))}
+            <section className='ftco-section ftco-counter section-counter-class' style={{ backgroundImage: 'url(\'' + this.state.statistic.background + '\')' }}>
+
+                <div className='text-center heading-section heading-section-white ftco-animate'>
+                    <h2 className='text'>{this.state.statistic.title}</h2>
+                    <span className='subheading text' dangerouslySetInnerHTML={{ __html: this.state.statistic.description }} />
+                </div>
+                <div className='row justify-content-center'>
+                    {this.state.statistic.items.map((item, index) => (
+                        <div key={index} className={itemClassName} >
+                            <div className="text-center milestone">
+                                <div className="milestone_counter">{item.number}</div>
+                                <div className="milestone_text">{item.title}</div>
                             </div>
                         </div>
-                    </div>
+                    ))}
                 </div>
+
+
             </section>) : '';
     }
 }
