acbede5b15342da6abe23ba5bc3209f30d0b6fbb
Trần Anh <trantuananh1001199999@gmail.com>
2021-03-26 11:06:13 +0700

    update

--- view/component/HomeMenu.jsx
+++ view/component/HomeMenu.jsx
@@ -152,11 +152,11 @@ class HomeMenu extends React.Component {
                             <ul className='d-flex flex-row align-items-center justify-content-start'>
                                 {user && user._id ?
                                     <div className='btn-group'>
-                                        <div className='button button_2 mr-1 hide_small'><a href='#'><i className='fa fa-phone' /> {mobile}</a></div>
-                                        <div className='button button_1 mr-1 hide_small' > <a href={DKTVLink}>Đăng ký tư vấn</a></div>
+                                        <div className='button button_2 mr-1 large_btn'><a href='#'><i className='fa fa-phone' /> {mobile}</a></div>
+                                        <div className='button button_1 mr-1 large_btn' > <a href={DKTVLink}>Đăng ký tư vấn</a></div>
                                         <div className="small-button">
-                                            <li data-toggle='tooltip' title='Số điện thoại'><a href='/user'><i className="fa fa-user-circle-o" aria-hidden="true"></i></a></li>
-                                            <li data-toggle='tooltip' title='Đăng xuất'><a href="#" onClick={this.logout} ><i className="fa fa-power-off" style={{ color: 'red' }} aria-hidden="true"></i></a></li>
+                                            <li data-toggle='tooltip' title='Số điện thoại'><a href={mobile}><i class="fa fa-phone-square" style={{ color: '#4ca758' }} aria-hidden="true"></i></a></li>
+                                            <li data-toggle='tooltip' title='Đăng ký tư vấn'><a href={DKTVLink}><i className="fa fa fa-envelope" style={{ color: 'red' }} aria-hidden="true"></i></a></li>
                                         </div>
                                         <div className='btn-group m-auto pl-2'>
                                             <li data-toggle='tooltip' title='Trang cá nhân'><a href='/user'><i className="fa fa-user-circle-o" aria-hidden="true"></i></a></li>
