98a369be3dac56dedc6e47f22338b6d28bfed155
hithanhtung <hithanhtung@gmail.com>
2021-03-26 11:46:00 +0700

    candidate

--- modules/mdDaoTao/fwStudent/index.jsx
+++ modules/mdDaoTao/fwStudent/index.jsx
@@ -1,25 +1,14 @@
 import React from 'react';
 import Loadable from 'react-loadable';
 import Loading from 'view/component/Loading';
-import { ajaxSelectCourseType, ajaxGetCourseType } from 'modules/mdDaoTao/fwCourseType/redux';
 import SectionAdvisoryForm from './sectionAdvisoryForm';
 
 export default {
     init: () => {
-        T.component['advisory form'] = {
-            render: (viewId) => <SectionAdvisoryForm viewId={viewId} />,
-            backgroundColor: '#fb3993',
-            adapter: ajaxSelectCourseType,
-            getItem: ajaxGetCourseType,
-        };
     },
     redux: {
     },
     routes: [
-        // {
-        //     path: '/user/candidate',
-        //     component: Loadable({ loading: Loading, loader: () => import('./adminCandidatePage') })
-        // },
         // {
         //     path: '/user/student',
         //     component: Loadable({ loading: Loading, loader: () => import('./adminStudentPage') })
