d436f20ce9e478ebd58bf8f3bf5fb585557b55de
Nguyen The Vinh <vinh.nguyen24.la@gmail.com>
2021-03-26 10:54:17 +0700

    pb

--- modules/_default/fwHome/sectionStatistic.jsx
+++ modules/_default/fwHome/sectionStatistic.jsx
@@ -37,7 +37,7 @@ class SectionStatistic extends React.Component {
         const itemLength = items ? items.length : 0,
             itemClassName = 'col-md-' + Math.round(12 / itemLength) + ' d-flex justify-content-center counter-wrap ftco-animate';
         return itemLength ? (
-            <section className='ftco-section ftco-counter section-counter-class'>
+            <section className='ftco-section ftco-counter section-counter-class pb-5'>
                 <div className=' heading-section heading-section-white ftco-animate'>
                     {titleVisible ? <h2 className='text'>{title}</h2> : null}
                     {description ? <span className='subheading text' dangerouslySetInnerHTML={{ __html: description }} /> : null}
