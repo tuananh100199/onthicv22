6b7b8a2e5928d8922e9ef5880277cceb6d5a7e87
hithanhtung <hithanhtung@gmail.com>
2021-03-27 11:52:43 +0700

    candidate reivew

--- modules/mdDaoTao/fwCandidate/sectionAdvisoryForm.jsx
+++ modules/mdDaoTao/fwCandidate/sectionAdvisoryForm.jsx
@@ -2,18 +2,28 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { ajaxSelectCourseType, ajaxGetCourseType } from 'modules/mdDaoTao/fwCourseType/redux';
 import { FormSelect } from 'view/component/AdminPage';
-import { createCandidate} from './redux';
+import { createCandidate } from './redux';
 
 class SectionAdvisoryForm extends React.Component {
+    state = {};
     componentDidMount() {
-        $(document).ready(() => this.props.viewId && ajaxGetCourseType(this.props.viewId, data =>
-            this.courseType.value(data && data.item ? { id: data.item._id, text: data.item.title } : null)));
+        $(document).ready(() => {
+            this.props.viewId && ajaxGetCourseType(this.props.viewId, data =>
+                this.courseType.value(data && data.item ? { id: data.item._id, text: data.item.title } : null));
+
             const { lastname, firstname, email, phoneNumber } = this.props.system && this.props.system.user ?
-            this.props.system.user : { lastname: '', firstname: '', email: '', phoneNumber: '' };
-            $('#userFirstname').val(firstname);  
-            $('#userLastname').val(lastname);
+                this.props.system.user : { lastname: '', firstname: '', email: '', phoneNumber: '' };
+            $('#userFirstname').val(firstname);
+            if (this.props.system && this.props.system.user) {
+                //TODO
+            } else {
+                this.lastname.value = lastname;
+            }
             $('#userEmail').val(email);
             $('#userPhone').val(phoneNumber);
+
+            this.setState({ lastname, firstname, email, phoneNumber });
+        });
     }
 
     onSubmit = (e) => {
@@ -39,7 +49,7 @@ class SectionAdvisoryForm extends React.Component {
                 phone: this.phone.value
             };
             this.props.createCandidate(data, () => {
-                this.firstname.value = this.lastname.value =  this.email.value = this.phone.value = '';
+                this.firstname.value = this.lastname.value = this.email.value = this.phone.value = '';
                 T.notify('Đăng ký tư vấn của bạn đã được gửi!', 'success', true, 3000);
             });
         }
@@ -47,6 +57,7 @@ class SectionAdvisoryForm extends React.Component {
 
     render() {
         const readOnly = this.props.system && this.props.system.user ? true : false;
+        const { lastname, firstname, email, phoneNumber } = this.state;
         return (
             <div className='intro'>
                 <div className='intro_col'>
@@ -54,11 +65,21 @@ class SectionAdvisoryForm extends React.Component {
                         <div className='intro_form_title' id='formTitle'>Đăng ký tư vấn</div>
                         <form action='#' className='intro_form' id='intro_form' onSubmit={this.onSubmit}>
                             <div className='d-flex flex-row align-items-start justify-content-between flex-wrap'>
-                                <input type='text' className='intro_input' placeholder='Họ' ref={e => this.lastname = e} required='required' id='userLastname' readOnly={readOnly}/>
-                                <input type='text' className='intro_input' placeholder='Tên' ref={e => this.firstname = e} required='required' id='userFirstname' readOnly={readOnly}/>
-                                <input type='text' className='intro_input' ref={e => this.email = e} placeholder='Email' id='userEmail' readOnly={readOnly}/>
-                                <input onKeyPress={e => (!/[0-9]/.test(e.key)) && e.preventDefault()}
-                                    type='tel' className='intro_input' placeholder='Số điện thoại' ref={e => this.phone = e} required='required' id='userPhone' readOnly={readOnly}/>
+                                {readOnly ?
+                                    <div className='w-100'>
+                                        <p style={{ width: '100%' }}>Họ và Tên: <b>{lastname} {firstname}</b></p>
+                                        <p style={{ width: '100%' }}>Email: <b>{email}</b></p>
+                                        <p style={{ width: '100%' }}>Số điện thoại:</p>
+                                        <input onKeyPress={e => (!/[0-9]/.test(e.key)) && e.preventDefault()}
+                                            type='tel' className='intro_input w-100' placeholder='Số điện thoại' ref={e => this.phone = e} required='required' id='userPhone' readOnly={readOnly} />
+                                    </div> :
+                                    <>
+                                        <input type='text' className='intro_input' placeholder='Họ' ref={e => this.lastname = e} required='required' />
+                                        <input type='text' className='intro_input' placeholder='Tên' ref={e => this.firstname = e} required='required' id='userFirstname' readOnly={readOnly} />
+                                        <input type='text' className='intro_input' ref={e => this.email = e} placeholder='Email' id='userEmail' readOnly={readOnly} />
+                                        <input onKeyPress={e => (!/[0-9]/.test(e.key)) && e.preventDefault()}
+                                            type='tel' className='intro_input' placeholder='Số điện thoại' ref={e => this.phone = e} required='required' id='userPhone' readOnly={readOnly} />
+                                    </>}
 
                                 <div className='intro_input w-100' style={{ padding: 0, border: 'none' }} >
                                     <FormSelect ref={e => this.courseType = e} label='' data={ajaxSelectCourseType} style={{ margin: 0, width: '100% !important' }} />
