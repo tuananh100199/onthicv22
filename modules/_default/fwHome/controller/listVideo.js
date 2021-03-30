fabe0d125e57b4538519e13ba43d760dfae9ef10
vantam10a11 <vantam10a11@gmail.com>
2021-03-30 10:24:16 +0700

    fix conntroler listvideo

--- modules/_default/fwHome/controller/listVideo.js
+++ modules/_default/fwHome/controller/listVideo.js
@@ -32,19 +32,6 @@ module.exports = app => {
     });
     app.delete('/api/list-video', app.permission.check('component:delete'), (req, res) => app.model.listVideo.delete(req.body._id, error => res.send({ error })));
 
-
-    app.post('/api/list-video/item', app.permission.check('component:write'), (req, res) => app.model.listVideo.create(req.body.data, (error, item) => {
-        res.send({ error, item });
-    }));
-
-    app.put('/api/list-video/item/swap', app.permission.check('component:write'), (req, res) => {
-        const isMoveUp = req.body.isMoveUp.toString() == 'true';
-        app.model.listVideo.swapPriority(req.body._id, isMoveUp, (error, item1, item2) => res.send({ error, item1, item2 }));
-    });
-
-    app.delete('/api/list-video/item', app.permission.check('component:write'), (req, res) =>
-        app.model.listVideo.delete(req.body._id, (error, item) => res.send({ error, listVideoId: item.listVideoId })));
-
     // Home ------------------------------------------------------------------------------------------------------------
     app.get('/home/list-video/:_id', (req, res) =>
         app.model.listVideo.get(req.params._id, (error, item) => res.send({ error, item })));
