a30fe1abe7ce8cc3d95cba634b48671e261556f2
Nguyen The Vinh <vinh.nguyen24.la@gmail.com>
2021-03-31 14:09:41 +0700

    delete nhieu cau hoi

--- modules/_default/fwQuestion/model.js
+++ modules/_default/fwQuestion/model.js
@@ -1,30 +1,17 @@
 module.exports = app => {
     const schema = app.db.Schema({
+        active: { type: Boolean, default: true },
         title: String,
         image: String,
         answers: { type: String, default: '' },
         trueAnswer: Number,
-        active: { type: Boolean, default: true },
     });
     const model = app.db.model('Question', schema);
 
     app.model.question = {
-        create: (data, done) => {
-            model.create(data, (error, item) => {
-                if (error) {
-                    done(error);
-                } else {
-                    item.image = '/img/question/' + item._id + '.jpg';
-                    const srcPath = app.path.join(app.publicPath, '/img/avatar.jpg'),
-                        destPath = app.path.join(app.publicPath, item.image);
-                    app.fs.copyFile(srcPath, destPath, error => error ? done(error) : item.save(done));
-                }
-            });
-        },
+        create: (data, done) => model.create(data, done),
 
-        getAll: (condition, done) => {
-            done ? model.find(condition).exec(done) : model.find({}).exec(condition);
-        },
+        getAll: (condition, done) => done ? model.find(condition).exec(done) : model.find({}).exec(condition),
 
         get: (condition, done) => {
             if (done == undefined) {
@@ -35,19 +22,35 @@ module.exports = app => {
             model.findOne(condition).exec(done);
         },
 
-        update: (_id, $set, $unset, done) => done ?
-            model.findOneAndUpdate({ _id }, { $set, $unset }, { new: true }, done) :
-            model.findOneAndUpdate({ _id }, { $set }, { new: true }, $unset),
+        // changes = { $set, $unset, $push, $pull }
+        update: (_id, changes, done) => model.findOneAndUpdate({ _id }, changes, { new: true }, done),
 
-        delete: (_id, done) => model.findOne({ _id }, (error, item) => {
-            if (error) {
-                done(error);
-            } else if (item == null) {
-                done('Invalid Id!');
-            } else {
-                app.deleteImage(item.image);
-                item.remove(done);
+        delete: (_id, done) => {
+            if (typeof _id == 'string') {
+                model.findOne({ _id }, (error, item) => {
+                    if (error) {
+                        done(error);
+                    } else if (item == null) {
+                        done('Invalid Id!');
+                    } else {
+                        app.deleteImage(item.image);
+                        item.remove(done);
+                    }
+                })
+            } else if (Array.isArray(_id)) {
+                _id.map((item, index) => {
+                    model.findOne({ _id: item._id }, (error, question) => {
+                        if (error) {
+                            done(error);
+                        } else if (question == null) {
+                            done('Invalid Id!');
+                        } else {
+                            app.deleteImage(question.image);
+                            index == _id.length - 1 ? question.remove(done) : question.remove();
+                        }
+                    })
+                });
             }
-        }),
+        }
     };
 };
