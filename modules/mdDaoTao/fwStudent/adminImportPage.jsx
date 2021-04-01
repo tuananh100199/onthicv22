58df0ccb69bcf26f330ebbb8f2993892c5b323a7
Nguyen The Vinh <vinh.nguyen24.la@gmail.com>
2021-04-01 14:54:33 +0700

    fix import

--- modules/mdDaoTao/fwStudent/adminImportPage.jsx
+++ modules/mdDaoTao/fwStudent/adminImportPage.jsx
@@ -1,12 +1,81 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { AdminPage, FormFileBox, TableCell, renderTable, CirclePageButton } from 'view/component/AdminPage';
+import { AdminPage, AdminModal, FormFileBox, FormTextBox, TableCell, renderTable, CirclePageButton } from 'view/component/AdminPage';
 
+
+class EditModal extends AdminModal {
+    state = {};
+    componentDidMount() {
+        $(document).ready(() => this.onShown(() => this.itemFirstname.focus()));
+    }
+
+    onShow = (item, index) => {
+        let { numberic, firstname, lastname, email, phoneNumber, courseType } = item || { firstname: '', lastname: '', email: '', phoneNumber: '', courseType: '' };
+        this.setState({ num: numberic })
+        this.itemFirstname.value(firstname);
+        this.itemLastname.value(lastname);
+        this.itemEmail.value(email);
+        this.itemPhoneNumber.value(phoneNumber);
+        this.itemCourseType.value(courseType);
+    }
+
+    onSubmit = () => {
+        const data = {
+            firstname: this.itemFirstname.value(),
+            lastname: this.itemLastname.value(),
+            email: this.itemEmail.value(),
+            phoneNumber: this.itemPhoneNumber.value(),
+            courseType: this.itemCourseType.value(),
+        };
+        if (data.firstname == '') {
+            T.notify('Tên ứng viên bị trống!', 'danger');
+            this.itemFirstname.focus();
+        } else if (data.lastname == '') {
+            T.notify('Tên ứng viên bị trống!', 'danger');
+            this.itemLastname.focus();
+        } else if (data.email == '') {
+            T.notify('Email ứng viên bị trống!', 'danger');
+            this.itemEmail.focus();
+        } else if (data.phoneNumber == '') {
+            T.notify('Số điện thoại ứng viên bị trống!', 'danger');
+            this.itemPhoneNumber.focus();
+        } else if (data.courseType == '') {
+            T.notify('Hạng đăng ký của ứng viên bị trống!', 'danger');
+            this.itemCourseType.focus();
+        } else {
+            this.hide();
+        }
+    }
+
+    render = () => this.renderModal({
+        title: 'Chỉnh sửa ứng viên',
+        size: 'large',
+        body: (
+            <div className='row'>
+                <div className='col-md-8'>
+                    <FormTextBox ref={e => this.itemFirstname = e} label='Họ ứng viên' readOnly={this.props.readOnly} />
+                    <FormTextBox ref={e => this.itemLastname = e} label='Tên ứng viên' readOnly={this.props.readOnly} />
+                    <FormTextBox ref={e => this.itemEmail = e} label='Email' readOnly={this.props.readOnly} />
+                    <FormTextBox ref={e => this.itemPhoneNumber = e} label='Số điện thoại' readOnly={this.props.readOnly} />
+                    <FormTextBox ref={e => this.itemCourseType = e} label='Hạng đăng ký' readOnly={this.props.readOnly} />
+                </div>
+            </div>),
+    });
+}
 class ImportPage extends AdminPage {
     fileBox = React.createRef();
     state = {};
     onUploadSuccess = (data) => {
         this.setState(data);
+    }
+    showEditModal = (e, item) => e.preventDefault() || this.modalEdit.show(item);
+    edit = (numberic, changes) => {
+        this.setState(prevState => ({
+            data: prevState.data.map(
+                data => data.numberic === numberic ? { ...data, changes } : data
+            )
+        }))
+        console.log(this.state.data)
     }
     render() {
         const permission = this.getUserPermission('student', ['read', 'write', 'delete']),
@@ -20,6 +89,7 @@ class ImportPage extends AdminPage {
                     <th style={{ width: '30%' }}>Email</th>
                     <th style={{ width: '20%' }}>Di động</th>
                     <th style={{ width: 'auto', textAlign: 'center' }} nowrap='true'>Hạng đăng ký</th>
+                    <th style={{ width: 'auto', textAlign: 'center' }} nowrap='true'>Thao tác</th>
                 </tr>),
             renderRow: (item, index) => (
                 <tr key={index}>
@@ -28,6 +98,7 @@ class ImportPage extends AdminPage {
                     <TableCell type='text' content={item.email} />
                     <TableCell type='text' content={T.mobileDisplay(item.phoneNumber)} />
                     <TableCell type='text' style={{ whiteSpace: 'nowrap' }} content={item.courseType} />
+                    <TableCell type='buttons' content={item} permission={permission} onEdit={this.showEditModal} onDelete={this.edit} />
                 </tr>),
         });
         return this.renderPage({
@@ -37,7 +108,7 @@ class ImportPage extends AdminPage {
             content: <>
                 <div className='tile'>
                     <h3 className='tile-title'>Upload danh sách ứng viên</h3>
-                    <FormFileBox ref={e => this.fileBox = e} className='col-md-3' label='File excel ứng viên' uploadType='CandidateFile'
+                    <FormFileBox ref={e => this.fileBox = e} className='col-md-6' label='File excel ứng viên' uploadType='CandidateFile'
                         onSuccess={this.onUploadSuccess} readOnly={readOnly} />
                 </div>
 
@@ -47,6 +118,7 @@ class ImportPage extends AdminPage {
                         {table}
                     </div>
                 </div>
+                <EditModal ref={e => this.modalEdit = e} readOnly={!permission.write} />
                 <CirclePageButton type='save' onClick={() => alert('todo')} />
             </>,
             backRoute: '/user/pre-student',
