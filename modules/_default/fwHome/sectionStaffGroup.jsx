5dfe7d1ffd58a2347dbd1dc7cbdd3dca3a8b3475
vantam10a11 <vantam10a11@gmail.com>
2021-04-07 16:52:08 +0700

    fix center image

--- modules/_default/fwHome/sectionStaffGroup.jsx
+++ modules/_default/fwHome/sectionStaffGroup.jsx
@@ -106,7 +106,7 @@ class SectionStaffGroup extends React.Component {
             </div>
             <div className='row'>
                 {this.state.items ? this.state.items.map((staff, index) => (
-                    <div key={index} className="col-md-6 col-lg-3 ftco-animate" style={{ margin: 'auto' }}>
+                    <div key={index} className="col-md-6 col-lg-3 ftco-animate" style={{ marginLeft: 'auto', marginRight: 'auto' }}>
                         <div className='text-center'>
                             <img style={{}} src={staff.image || staff.user.image} height={100} alt='' />
                             <div className='service_title'>{staff.user.lastname + ' ' + staff.user.firstname}</div>
