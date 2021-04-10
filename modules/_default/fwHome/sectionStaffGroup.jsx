c1f11fe994c932049fdd0f831639720633f3e7fa
vantam10a11 <vantam10a11@gmail.com>
2021-04-10 13:11:38 +0700

    fix display image in section StaffGroup

--- modules/_default/fwHome/sectionStaffGroup.jsx
+++ modules/_default/fwHome/sectionStaffGroup.jsx
@@ -22,10 +22,12 @@ class SectionStaffGroup extends React.Component {
             <div className='row'>
                 {this.state.items ? this.state.items.map((staff, index) => (
                     <div key={index} className="col-md-6 col-lg-3 team_col ftco-animate" style={{ marginLeft: 'auto', marginRight: 'auto' }}>
+                        {/* // <div key={index} className={this.state.items.length == 4 ? "col-md-6 col-lg-3 team_col ftco-animate" : "col-md-6 col-lg-4 team_col ftco-animate"} style={{ marginLeft: 'auto', marginRight: 'auto' }}> */}
                         <div className="team_item text-center d-flex flex-column aling-items-center justify-content">
                             <div className="team_image" style={{ height: 300 }}><img src={staff.image || staff.user.image} alt='' /></div>
                             <div className="team_content text-center">
-                                <div className='team_title'>{staff.user.lastname + ' ' + staff.user.firstname}</div>
+                                <div className='team_name' style={{ whiteSpace: 'nowrap' }}>{staff.user.lastname + ' ' + staff.user.firstname}</div>
+                                <div className='team_title' style={{ height: 10 }}>{staff.user.isLecturer && 'Giáo viên' || staff.user.isStaff && 'Nhân viên'}</div>
                                 <div className="team_text">
                                     <blockquote>
                                         <p>&ldquo;{staff.description}&rdquo;</p>
