87691940371789f9b15800881e7a436cab2f332c
hithanhtung <hithanhtung@gmail.com>
2021-04-13 07:34:00 +0700

    chỉ thêm môn học chưa có vào trong CourseType

--- modules/mdDaoTao/fwCourseType/adminEditPage.jsx
+++ modules/mdDaoTao/fwCourseType/adminEditPage.jsx
@@ -6,7 +6,7 @@ import { ajaxSelectSubject } from 'modules/mdDaoTao/fwSubject/redux';
 import { AdminPage, CirclePageButton, AdminModal, FormTextBox, FormRichTextBox, FormEditor, FormImageBox, TableCell, renderTable, FormCheckbox, FormTabs, FormSelect } from 'view/component/AdminPage';
 
 class CourseTypeModal extends AdminModal {
-    onShow = () => this.subjectSelect.value('');
+    onShow = () => this.subjectSelect.value(null);
 
     onSubmit = () => {
         const _subjectId = this.subjectSelect.value();
@@ -17,10 +17,20 @@ class CourseTypeModal extends AdminModal {
         }
     }
 
-    render = () => this.renderModal({
-        title: 'Môn học',
-        body: <FormSelect ref={e => this.subjectSelect = e} label='Môn học' data={ajaxSelectSubject} readOnly={this.props.readOnly} />
-    });
+    render = () => {
+        // chỉ lấy các môn chưa đưa vào
+        const processResults = response => {
+            const _subjectIds = this.props.item.subjects.map(item => item._id),
+                results = [];
+            (response && response.page && response.page.list ? response.page.list : [])
+                .forEach(item => _subjectIds.includes(item._id) || results.push({ id: item._id, text: item.title }));
+            return { results }
+        };
+        return this.renderModal({
+            title: 'Môn học',
+            body: <FormSelect ref={e => this.subjectSelect = e} label='Môn học' data={{ ...ajaxSelectSubject, processResults }} readOnly={this.props.readOnly} />
+        });
+    };
 }
 
 const backRoute = '/user/course-type'
