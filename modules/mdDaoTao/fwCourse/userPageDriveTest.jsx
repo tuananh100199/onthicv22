7b24585d0ef33b9d7f47851e492992e25d111534
Trần Anh <trantuananh1001199999@gmail.com>
2021-04-24 13:05:09 +0700

    Làm phần câu hỏi thi trên pC

--- modules/mdDaoTao/fwCourse/userPageDriveTest.jsx
+++ modules/mdDaoTao/fwCourse/userPageDriveTest.jsx
@@ -32,10 +32,25 @@ class UserPageDriveTest extends AdminPage {
             this.setState({ result: result })
         })
     }
+    changeQuestion = (index) => {
+        if(index == 0) {
+            $('#prev-btn').attr('disabled', true);
+        } else if(index == this.state.questions.length()-1) {
+            $('#next-btn').attr('disabled', true);
+        } else {
+            this.setState({activeQuestionIndex: index});
+        }
+    }
 
     render() {
         const { questions } = this.state ? this.state : { questions: [] };
-        const { score, total } = this.state.result ? this.state.result : { score: 0, total: questions && questions.length };
+        const activeQuestionIndex = this.state.activeQuestionIndex ? this.state.activeQuestionIndex : 0;
+        console.log('this.state',this.state)
+        console.log('activeQuestionIndex',activeQuestionIndex)
+
+        // const { score, total } = this.state.result ? this.state.result : { score: 0, total: questions && questions.length };
+        const activeQuestion = questions ? questions[activeQuestionIndex + 1] : null;
+        console.log('activeQuestion', questions&&questions[0])
 
         return this.renderPage({
             icon: 'fa fa-dashboard',
@@ -44,16 +59,16 @@ class UserPageDriveTest extends AdminPage {
             content: (
                 <div className='tile'>
                     <div className='tile-body row'>
-                        {questions ? questions.map((question, indexQuestion) => question.active ?
+                        {activeQuestion? 
                             (
-                                <div key={indexQuestion} className='col-md-6 pb-5'>
-                                    <h6>Câu {indexQuestion + 1}:{question.title}</h6>
-                                    {question.image ? <img src={question.image} alt='question' style={{ width: '50%', height: 'auto' }} /> : null}
+                                <div className='col-md-12 pb-5'>
+                                    <h6>Câu hỏi:{activeQuestion.title}</h6>
+                                    {activeQuestion.image ? <img src={activeQuestion.image} alt='question' style={{ width: '50%', height: 'auto' }} /> : null}
                                     <div className='form-check'>
-                                        {question.answers.split('\n').map((answer, index) => (
+                                        {activeQuestion.answers.split('\n').map((answer, index) => (
                                             <div key={index}>
-                                                <input className='form-check-input' type='radio' name={question._id} id={question._id + index} value={index} />
-                                                <label className='form-check-label' htmlFor={question._id + index}>
+                                                <input className='form-check-input' type='radio' name={activeQuestion._id} id={activeQuestion._id + index} value={index} />
+                                                <label className='form-check-label' htmlFor={activeQuestion._id + index}>
                                                     {answer}
                                                 </label>
                                             </div>
@@ -61,11 +76,21 @@ class UserPageDriveTest extends AdminPage {
                                     </div>
                                 </div>
                             ) : <></>
-                        ): null}
+                        }
                     </div>
                     <div className='tile-footer' style={{ display: 'flex', justifyContent: 'space-around' }}>
-                        <button className='btn btn-primary' onClick={e => this.submitAnswer(e, questions)}>Gửi</button>
-                        <p>Số câu đúng của bạn: <b>{score} / {total}</b></p>
+                    <nav aria-label='...'>
+                        <ul className='pagination'>
+                            <li className='page-item disabled' id='prev-btn'>
+                                <a className='page-link' onClick={this.changeQuestion(activeQuestionIndex)}><i className='fa fa-arrow-left' aria-hidden='true'></i> Câu trước</a>
+                            </li>
+                            <li className='page-item' id='next-btn'>
+                                <a className='page-link' href='#' tabIndex='1'> Câu tiếp <i className='fa fa-arrow-right' aria-hidden='true'></i></a>
+                            </li>
+                        </ul>
+                        </nav>
+                        {/* <button className='btn btn-primary' onClick={e => this.submitAnswer(e, questions)}>Gửi</button> */}
+                        {/* <p>Số câu đúng của bạn: <b>{score} / {total}</b></p> */}
                     </div>
                 </div>
             ),
