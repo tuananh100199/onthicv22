22e1dc7da121a7bc960ca1a7ea79f0ed8ff46b04
Trần Anh <trantuananh1001199999@gmail.com>
2021-05-19 10:55:37 +0700

    update

--- view/component/HomeMenu.jsx
+++ view/component/HomeMenu.jsx
@@ -136,11 +136,11 @@ class HomeMenu extends React.Component {
                                 {user && user._id ?
                                     <div className='btn-group'>
                                         <div className='button button_2 mr-1 large_btn'><a href={'tel:' + mobile}><i className='fa fa-phone' /> {mobile}</a></div>
-                                        <div className='button button_1 mr-1 large_btn' > <a onClick={this.showCandidateModal}>Đăng ký tư vấn</a></div>
+                                        <div className='button button_1 mr-1 large_btn' > <a href='#' onClick={this.showCandidateModal}>Đăng ký tư vấn</a></div>
 
                                         <div className='btn-group m-auto pl-2 small_btn' >
                                             <li data-toggle='tooltip' title='Số điện thoại'><a href={'tel:' + mobile}><i className='fa fa-phone' style={{ color: '#4CA758' }} /></a></li>
-                                            <li data-toggle='tooltip' title='Đăng ký tư vấn'><a onClick={this.showCandidateModal} ><i className='fa fa-envelope-o' style={{ color: 'red' }} aria-hidden='true'></i></a></li>
+                                            <li data-toggle='tooltip' title='Đăng ký tư vấn'><a href='#' onClick={this.showCandidateModal} ><i className='fa fa-envelope-o' style={{ color: 'red' }} aria-hidden='true'></i></a></li>
                                         </div>
                                         <div className='btn-group m-auto pl-2' >
                                             <li data-toggle='tooltip' title='Trang cá nhân'><a href='/user'><i className='fa fa-user-circle-o' style={{ color: '#4CA758' }} aria-hidden='true'></i></a></li>
@@ -149,12 +149,12 @@ class HomeMenu extends React.Component {
                                     </div> :
                                     <div className='btn-group'>
                                         <div className='button button_2 mr-1 large_btn'><a href={'tel:' + mobile}><i className='fa fa-phone' /> {mobile}</a></div>
-                                        <div className='button button_1 mr-1 large_btn' > <a onClick={this.showCandidateModal}>Đăng ký tư vấn</a></div>
+                                        <div className='button button_1 mr-1 large_btn' > <a href='#' onClick={this.showCandidateModal}>Đăng ký tư vấn</a></div>
                                         <div className='button button_2 mr-1 large_btn'><a href='#' onClick={this.props.showLoginModal}>Đăng nhập</a></div>
 
                                         <div className='btn-group m-auto pl-2 small_btn' >
                                             <li data-toggle='tooltip' title='Số điện thoại'><a href={'tel:' + mobile}><i className='fa fa-phone' style={{ color: '#4CA758' }} /></a></li>
-                                            <li data-toggle='tooltip' title='Đăng ký tư vấn'><a onClick={this.showCandidateModal} ><i className='fa fa-envelope-o' style={{ color: 'red' }} aria-hidden='true'></i></a></li>
+                                            <li data-toggle='tooltip' title='Đăng ký tư vấn'><a href='#' onClick={this.showCandidateModal} ><i className='fa fa-envelope-o' style={{ color: 'red' }} aria-hidden='true'></i></a></li>
                                             <li data-toggle='tooltip' title='Đăng nhập' className='login_css_small' ><a href='#' onClick={this.props.showLoginModal} ><i className='fa fa-user-circle-o' style={{ color: '#4CA758' }} /></a></li>
                                         </div>
                                     </div>}
