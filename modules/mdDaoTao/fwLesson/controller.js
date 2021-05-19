4a52fbd6058ce19d670661cd10eb5dc930ac6b4a
Nguyen The Vinh <vinh.nguyen24.la@gmail.com>
2021-05-19 17:31:57 +0700

    fix

--- modules/mdDaoTao/fwLesson/controller.js
+++ modules/mdDaoTao/fwLesson/controller.js
@@ -62,6 +62,8 @@ module.exports = (app) => {
                             }
                         }
                     });
+                } else {
+                    res.send({ error, item });
                 }
             }
         });
