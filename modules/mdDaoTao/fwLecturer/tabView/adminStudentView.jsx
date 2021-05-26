e559b529d0f1d6105f48107bca8fbcbde729af2a
Nguyen The Vinh <vinh.nguyen24.la@gmail.com>
2021-05-26 17:12:51 +0700

    fwLecturer

---
+++ modules/mdDaoTao/fwLecturer/tabView/adminStudentView.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { updateCourseStudents, getStudentByLecturer } from '../../fwCourse/redux';
+import { AdminPage, renderTable, TableCell } from 'view/component/AdminPage';
+
+class AdminStudentView extends AdminPage {
+    state = { searchPreStudentText: '', searchPreStudentCourse: '', searchStudentText: '', sortType: 'name', assignedButtonVisible: false }; // sortType = name | division
+    preStudents = {};
+    componentDidMount() {
+        // this.onSearch({}, { division: 0 });
+        this.props.getStudentByLecturer(this.props.courseId, data => {
+            this.setState({ listStudent: data.item });
+        });
+    }
+
+    // onSearch = ({ pageNumber, pageSize, searchText, searchPlanCourse }, sort, done) => {
+    //     if (searchText == undefined) searchText = this.state.searchPreStudentText;
+    //     if (searchPlanCourse == undefined) searchPlanCourse = this.state.searchPreStudentCourse;
+    //     if (sort == undefined) sort = { lastname: 1, firstname: 1 };
+    //     if (sort.division == undefined) sort.division = this.state.sortType == 'division' ? 1 : 0;
+    //     this.props.getPreStudentPage(pageNumber, pageSize, { searchText, searchPlanCourse, courseType: this.props.courseType && this.props.courseType._id }, sort, () => {
+    //         this.setState({
+    //             searchPreStudentText: searchText,
+    //             searchPreStudentPlanCourse: searchPlanCourse,
+    //             sortType: sort.division ? 'division' : 'name',
+    //             assignedButtonVisible: false,
+    //         });
+    //         done && done();
+    //     });
+    // }
+
+    render() {
+        const data = this.state.listStudent ? this.state.listStudent : [];
+        const table = renderTable({
+            getDataSource: () => data, stickyHead: true,
+            renderHead: () => (
+                <tr>
+                    <th style={{ width: 'auto', textAlign: 'center' }}>#</th>
+                    <th style={{ width: '100%' }}>Tên học viên</th>
+                </tr>),
+            renderRow: (item, index) => (
+                <tr key={index}>
+                    <TableCell type='number' content={index + 1} />
+                    <TableCell type='text' content={item.lastname + ' ' + item.firstname} />
+                </tr>),
+        });
+        return (
+            <div className='tile-body'>
+                {table}
+            </div>);
+    }
+}
+
+const mapStateToProps = state => ({ system: state.system, course: state.trainning.course });
+const mapActionsToProps = { updateCourseStudents, getStudentByLecturer };
+export default connect(mapStateToProps, mapActionsToProps)(AdminStudentView);
