baa44ec6b5cceae6ddf7f2357a65733bf9ca5fde
hithanhtung <hithanhtung@gmail.com>
2021-10-21 20:03:31 +0700

    merge tạm

--- modules/_default/fwNotification/controller.js
+++ modules/_default/fwNotification/controller.js
@@ -1,57 +1,58 @@
 module.exports = (app) => {
-    const menu = {
-        parentMenu: app.parentMenu.trainning,
-        menus: {
-            2200: { title: 'Thông báo', link: '/user/notification' },
-        }
-    };
-
     app.permission.add(
-        { name: 'notification:read', menu },
+        { name: 'user:login', menu: { parentMenu: { index: 1100, title: 'Thông báo', icon: 'fa-comment', link: '/user/notification' } } },
         { name: 'notification:write' },
         { name: 'notification:delete' },
     );
 
-    app.get('/user/notification', app.permission.check('notification:read'), app.templates.admin);
+    app.get('/user/notification', app.permission.check('user:login'), app.templates.admin);
 
     // APIs ------------------------------------------------------------------------------------------------------------
     app.get('/api/notification/page/:pageNumber/:pageSize', (req, res) => {
         const pageNumber = parseInt(req.params.pageNumber),
             pageSize = parseInt(req.params.pageSize);
-        app.model.courseType.getPage(pageNumber, pageSize, {}, (error, page) => {
+        app.model.notification.getPage(pageNumber, pageSize, {}, (error, page) => {
             res.send({ page, error: error ? 'Danh sách thông báo không sẵn sàng!' : null });
         });
     });
 
-    app.get('/api/notification', (req, res) => {
-        app.model.courseType.get(req.query._id, (error, item) => res.send({ error, item }));
+    app.post('/api/notification', app.permission.check('user:login'), (req, res) => {
+        const user = req.session.user,
+            permissions = user && user.permissions && user.permissions.length ? user.permissions : [];
+        if (user.isCourseAdmin || permissions.includes('notification:write')) {
+            app.model.notification.create(req.body.data, (error, item) => res.send({ error, item }));
+        } else {
+            res.send({ error: 'Bạn không có quyền!' });
+        }
     });
 
-    app.post('/api/notification', app.permission.check('notification:write'), (req, res) => {
-        app.model.courseType.create(req.body.data, (error, item) => res.send({ error, item }));
-    });
-
-    app.put('/api/notification', app.permission.check('notification:write'), (req, res) => {
-        const { _id, changes } = req.body;
-        if (changes && changes.subjects && changes.subjects === 'empty') changes.subjects = [];
-        app.model.courseType.update(_id, changes, (error, item) => res.send({ error, item }));
+    app.put('/api/notification', app.permission.check('user:login'), (req, res) => {
+        const user = req.session.user,
+            permissions = user && user.permissions && user.permissions.length ? user.permissions : [];
+        if (user.isCourseAdmin || permissions.includes('notification:write')) {
+            const { _id, changes } = req.body;
+            if (changes && changes.subjects && changes.subjects === 'empty') changes.subjects = [];
+            app.model.notification.update(_id, changes, (error, item) => res.send({ error, item }));
+        } else {
+            res.send({ error: 'Bạn không có quyền!' });
+        }
     });
 
     app.delete('/api/notification', app.permission.check('notification:delete'), (req, res) => {
         const { _id } = req.body;
-        app.model.courseType.delete(_id, (error) => res.send({ error }));
+        app.model.notification.delete(_id, (error) => res.send({ error }));
     });
 
     // Hook upload images ---------------------------------------------------------------------------------------------
     app.createFolder(app.path.join(app.publicPath, '/img/notification'));
 
-    const uploadCourseType = (fields, files, done) => {
-        if (fields.userData && fields.userData[0].startsWith('notification:') && files.CourseTypeImage && files.CourseTypeImage.length > 0) {
-            console.log('Hook: uploadCourseType => course type image upload');
+    const uploadNotification = (fields, files, done) => {
+        if (fields.userData && fields.userData[0].startsWith('notification:') && files.NotificationImage && files.NotificationImage.length > 0) {
+            console.log('Hook: uploadNotification => course type image upload');
             const _id = fields.userData[0].substring('notification:'.length);
-            app.uploadImage('notification', app.model.courseType.get, _id, files.CourseTypeImage[0].path, done);
+            app.uploadImage('notification', app.model.notification.get, _id, files.NotificationImage[0].path, done);
         }
     };
-    app.uploadHooks.add('uploadCourseType', (req, fields, files, params, done) =>
-        app.permission.has(req, () => uploadCourseType(fields, files, done), done, 'notification:write'));
+    app.uploadHooks.add('uploadNotification', (req, fields, files, params, done) =>
+        app.permission.has(req, () => uploadNotification(fields, files, done), done, 'notification:write'));
 };
--- modules/_default/fwNotification/model.js
+++ modules/_default/fwNotification/model.js
@@ -3,16 +3,28 @@ module.exports = app => {
         type: String,                                           // Loại notification
         title: String,
         content: String,
-        image: String,
-        active: { type: Boolean, default: true },
         createdDate: { type: Date, default: Date.now },
+        sentDate: { type: Date, default: null },
     });
     const model = app.db.model('Notification', schema);
 
     app.model.notification = {
         create: (data, done) => model.create(data, done),
 
-        getAll: (condition, done) => model.find(condition).sort({ createdDate: -1 }).exec(done),
+        getPage: (pageNumber, pageSize, condition, done) => model.countDocuments(condition, (error, totalItem) => {
+            if (error) {
+                done(error);
+            } else {
+                const result = { totalItem, pageSize, pageTotal: Math.ceil(totalItem / pageSize) };
+                result.pageNumber = pageNumber === -1 ? result.pageTotal : Math.min(pageNumber, result.pageTotal);
+
+                const skipNumber = (result.pageNumber > 0 ? result.pageNumber - 1 : 0) * result.pageSize;
+                model.find(condition).sort({ createdDate: -1 }).skip(skipNumber).limit(result.pageSize).exec((error, list) => {
+                    result.list = list;
+                    done(error, result);
+                });
+            }
+        }),
 
         get: (condition, done) => typeof condition == 'string' ? model.findById(condition, done) : model.findOne(condition, done),
 
@@ -26,7 +38,7 @@ module.exports = app => {
             } else if (item == null) {
                 done('Invalid Id!');
             } else {
-                app.deleteImage(item.image);
+                item.image && app.deleteImage(item.image);
                 item.remove(done);
             }
         }),
