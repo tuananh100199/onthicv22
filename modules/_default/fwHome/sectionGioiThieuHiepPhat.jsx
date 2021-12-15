2bb4b6f8c5ff0f7e66bb9b2f6ad0951b99c1e12d
Nguyen The Vinh <vinh.nguyen24.la@gmail.com>
2021-12-15 09:01:24 +0700

    fix gioi thieu hiep phat

--- modules/_default/fwHome/sectionGioiThieuHiepPhat.jsx
+++ modules/_default/fwHome/sectionGioiThieuHiepPhat.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { getGioiThieu } from './redux/reduxGioiThieuHiepPhat';
 import './style.css';
 
 class SectionGioiThieuHiepPhat extends React.Component {
+    state = {};
+    componentDidMount() {
+        if (this.props.viewId) {
+            this.props.getGioiThieu(this.props.viewId, data => {
+                data && data.item && this.setState(data.item);
+            });
+        }
+    }
     render() {
+        const { image1, image2, image3, abstract, title } = this.state;
         return (
             <div className='intro'>
                 <div className='intro_col'>
@@ -13,14 +23,10 @@ class SectionGioiThieuHiepPhat extends React.Component {
                                 <div className='col-lg-6 col-md-12'>
                                     <div className='title_gioi_thieu'>
                                         Giới thiệu
-                                        <span>TRUNG TÂM HIỆP PHÁT</span>
+                                        <span>{title}</span>
                                     </div>
                                     <div className='description'>
-                                        Trung tâm dạy nghề lái xe HIỆP PHÁT hiểu rõ nhu cầu của học viên
-                                        muốn học lái xe với trung tâm đào tạo lái xe uy tín nhất, được giảng dạy tận tâm
-                                        và nhiệt tình nhất như: được giảng dạy lý thuyết dễ hiểu nắm vững kiến thức Luật Giao
-                                        Thông Đường Bộ, ôn luyện trắc nghiệm câu hỏi Luật trên máy tính và quan trọng nhất đối
-                                        với mỗi học viên khi tham gia các lớp học lái xe của trung tâm là xe tập lái <span>Xem thêm &gt;&gt;</span>
+                                        {abstract}<a href='/ve-chung-toi'> Xem thêm &gt;&gt;</a>
                                     </div>
                                     <div className='social_gioi_thieu'>
                                         <ul className='social-menu'>
@@ -33,13 +39,14 @@ class SectionGioiThieuHiepPhat extends React.Component {
                                 </div>
                                 <div className='col-lg-6 col-md-12'>
                                     <div>
-                                        <span className='img-intro1'>Red</span>
+                                        {/* <span className='img-intro1'>Red</span> */}
+                                        <img src={image1} className='img-intro1' alt='no image'></img>
                                     </div>
                                     <div>
-                                        <span className='img-intro2'>Green</span>
+                                        <img src={image2} className='img-intro2' alt='no image'></img>
                                     </div>
                                     <div>
-                                        <span className='img-intro3'>Blue</span>
+                                        <img src={image3} className='img-intro3' alt='no image'></img>
                                     </div>
                                 </div>
                             </div>
@@ -52,5 +59,5 @@ class SectionGioiThieuHiepPhat extends React.Component {
 }
 
 const mapStateToProps = state => ({ system: state.system });
-const mapActionsToProps = {};
+const mapActionsToProps = { getGioiThieu };
 export default connect(mapStateToProps, mapActionsToProps)(SectionGioiThieuHiepPhat);
