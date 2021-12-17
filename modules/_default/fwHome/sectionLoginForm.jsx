650cde6457a2e17305ef3daf047f5e8013cc125a
Ricky Tran <ricky@boostcommerce.net>
2021-12-17 09:02:03 +0700

    sửa form đăng nhập

--- modules/_default/fwHome/sectionLoginForm.jsx
+++ modules/_default/fwHome/sectionLoginForm.jsx
@@ -18,9 +18,10 @@ class SectionLoginForm extends React.Component {
 
         if (data.username !== '' && data.password !== '') {
             this.props.login(data, result => {
-                this.errorMessage.innerHTML = result.error;
+                if (result.error) {
+                    this.errorMessage.innerHTML = result.error;
+                }
                 if (result.user) {
-                    $(this.modal.current).modal('hide');
                     window.location = '/user';
                 }
             });
@@ -38,37 +39,39 @@ class SectionLoginForm extends React.Component {
             <div className='intro login_form'>
                 <div className='intro_col'>
                     <div className='intro_form_container'>
-                            {item ? 
-                                <div className='row'>
-                                    <div className='col-md-7' style={{ width: '100%'}}>
-                                        <div className='wrap_image'>
-                                            <img src={item.image} style={{ width: '100%', objectFit: 'contain' }} alt='Image' />                                        
-                                        </div>
+                        {item ? 
+                            <div className='row'>
+                                <div className='col-md-7' style={{ width: '100%'}}>
+                                    <div className='wrap_image'>
+                                        <img src={item.image} style={{ width: '100%', objectFit: 'contain' }} alt='Image' />                                        
                                     </div>
-                                    <div className='col-md-5'>
-                                        <div className='form_title'>
-                                            {item.title}
-                                        </div>
-                                        <p className='content'>{item.content}</p>
-                                        <form action='#' className='intro_form' id='intro_form' onSubmit={this.onLogin}>
-                                            <div className='d-flex flex-row align-items-start justify-content-between flex-wrap'>
-                                                <div style={{ width: '100%', padding: '0 8px', margin: 0 }} className='wrap-box'>
-                                                    <div className='wrap_icon'>
-                                                        <i className='fa fa-user' aria-hidden='true' />
-                                                    </div>
-                                                    <input id='username' type='text' onKeyPress={e => (!/[0-9]/.test(e.key)) && e.preventDefault()} placeholder='Tên đăng nhập' ref={e => this.username = e} />
+                                </div>
+                                <div className='col-md-5'>
+                                    <div className='form_title'>
+                                        {item.title}
+                                    </div>
+                                    <p className='content'>{item.content}</p>
+                                    <form action='#' className='intro_form' id='intro_form' onSubmit={this.onLogin}>
+                                        <div className='d-flex flex-row align-items-start justify-content-between flex-wrap'>
+                                            <div style={{ width: '100%', padding: '0 8px', margin: 0 }} className='wrap-box'>
+                                                <div className='wrap_icon'>
+                                                    <i className='fa fa-user' aria-hidden='true' />
                                                 </div>
-                                                <div style={{ width: '100%', padding: '0 8px', margin: 0 }} className='wrap-box' >
-                                                    <div className='wrap_icon'>
-                                                        <i className='fa fa-lock' aria-hidden='true' />
-                                                    </div>
-                                                    <input id='password' type='text' placeholder='Mật khẩu' ref={e => this.password = e} />
-                                                </div>
+                                                <input id='username' type='text' onKeyPress={e => (!/[0-9]/.test(e.key)) && e.preventDefault()} placeholder='Tên đăng nhập' ref={e => this.username = e} />
                                             </div>
-                                            <button className='button button_1 intro_button trans_200' style={{ marginTop: '20px'}}>Đăng nhập</button>
-                                        </form>
-                                    </div>
-                                </div> : null}
+                                            <div style={{ width: '100%', padding: '0 8px', margin: 0 }} className='wrap-box' >
+                                                <div className='wrap_icon'>
+                                                    <i className='fa fa-lock' aria-hidden='true' />
+                                                </div>
+                                                <input id='password' type='text' placeholder='Mật khẩu' ref={e => this.password = e} />
+                                            </div>
+                                        </div>
+                                        <p ref={e => this.errorMessage = e} className='text-danger text-center'></p>
+                                        <button className='button button_1 intro_button trans_200' style={{ marginTop: '20px'}}>Đăng nhập</button>
+                                    </form>
+                                </div>
+                            </div> 
+                        : null}
                     </div>
                 </div>
             </div> :  null}
