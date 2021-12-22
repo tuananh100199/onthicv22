570b928c1df52a64fd4a05fd552cc88d403118ac
vinhnguyen24 <vinh,nguyen24@hcmut.edu.vn>
2021-12-22 15:32:21 +0700

    fix

--- modules/_default/_init/adminSettingCapturePage.jsx
+++ modules/_default/_init/adminSettingCapturePage.jsx
@@ -3,19 +3,7 @@ import { connect } from 'react-redux';
 import { getCaptureSetting, updateCaptureSetting } from './redux';
 import { AdminPage, FormTextBox } from 'view/component/AdminPage';
 import Webcam from 'react-webcam';
-// import ReactPlayer from 'react-player';
-// import '@tensorflow/tfjs-node';
-
-// // implements nodejs wrappers for HTMLCanvasElement, HTMLImageElement, ImageData
-// import * as canvas from 'canvas';
-
-// import * as faceapi from 'face-api.js';
-
-// // patch nodejs environment, we need to provide an implementation of
-// // HTMLCanvasElement and HTMLImageElement, additionally an implementation
-// // of ImageData is required, in case you want to use the MTCNN
-// const { Canvas, Image, ImageData } = canvas;
-// faceapi.env.monkeyPatch({ Canvas, Image, ImageData });
+import * as faceapi from 'face-api.js';
 
 class SettingsPage extends AdminPage {
     state = {};
@@ -40,8 +28,14 @@ class SettingsPage extends AdminPage {
 
     capture = (e) => {
         e.preventDefault;
-        const imageSrc = this.webcam.getScreenshot();
-        console.log(imageSrc);
+        // const imageSrc = this.webcam.getScreenshot();
+        Promise.all([
+            faceapi.nets.tinyFaceDetector.loadFromUri('/public/document')
+        ]).then(() => {
+            // const detection = faceapi.detectAllFaces(imageSrc, 
+            //     new faceapi.TinyFaceDetectorOptions());
+            console.log('a');    
+        });
     }
 
     render() {
