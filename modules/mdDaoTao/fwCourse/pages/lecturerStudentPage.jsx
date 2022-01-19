4809e8e6677c74fea2e6042b83d05fc115233b7e
Nguyen The Vinh <vinh.nguyen24.la@gmail.com>
2022-01-19 15:56:00 +0700

    them mot so cot trang hoc vien cua ban

--- modules/mdDaoTao/fwCourse/pages/lecturerStudentPage.jsx
+++ modules/mdDaoTao/fwCourse/pages/lecturerStudentPage.jsx
@@ -29,34 +29,21 @@ class LecturerStudentPage extends AdminPage {
         });
     }
 
-    checkMonLyThuyet = (student, subjects) => {
+    checkMonLyThuyet = (student, subject) => {
         const tienDoThiHetMon = student && student.tienDoThiHetMon;
-        const monLyThuyet = subjects.filter(subject => subject.monThucHanh == false);
-        const listIdThiHetMon = tienDoThiHetMon ? Object.keys(tienDoThiHetMon) : [];
-        let lyThuyet = true;
-        if (listIdThiHetMon.length) {
-            for (let i = 0; i < monLyThuyet.length; i++) {
-                const index = listIdThiHetMon.findIndex(monThi => monThi == monLyThuyet[i]._id);
-                if( index == -1){
-                    lyThuyet = false;
-                    break;
-                } else{
-                    if(!(tienDoThiHetMon[listIdThiHetMon[index]].score && (parseInt(tienDoThiHetMon[listIdThiHetMon[index]].score) >= 5))){
-                        lyThuyet = false;
-                        break;
-                    }
-                }
-            }
-        } else {
-            lyThuyet = false;
-        }
-        return lyThuyet;
+        const result = tienDoThiHetMon[subject._id] && tienDoThiHetMon[subject._id].score && parseInt(tienDoThiHetMon[subject._id].score) >= 5;
+        return result;
     }
 
     render() {
         const item = this.props.course && this.props.course.item ? this.props.course.item : {},
-        students = this.props.course && this.props.course && this.props.course.students ? this.props.course.students : [],
-        subjects = this.props.course && this.props.course.subjects ? this.props.course.subjects.sort((a, b) => a.monThucHanh - b.monThucHanh) : [];
+            students = this.props.course && this.props.course && this.props.course.students ? this.props.course.students : [],
+            subjects = this.props.course && this.props.course.subjects ? this.props.course.subjects.sort((a, b) => a.monThucHanh - b.monThucHanh) : [];
+        const monLyThuyet = subjects.filter(subject => subject.monThucHanh == false);
+        const subjectColumns = [];
+        (monLyThuyet || []).forEach((subject, index) => {
+            subjectColumns.push(<th key={index} style={{ width: 'auto', whiteSpace: 'pre', textAlign: 'center' }}  >{subject.title}</th>);
+        });
         // const teacherGroup = item && item.teacherGroups ? item.teacherGroups.find(group => group.teacher && group.teacher._id == currentUser._id) : null;
         const table = renderTable({
             getDataSource: () => students, stickyHead: true,
@@ -66,9 +53,10 @@ class LecturerStudentPage extends AdminPage {
                     <th style={{ width: '100%' }}>Tên học viên</th>
                     <th style={{ width: 'auto' }} nowrap='true'>CMND/CCCD</th>
                     <th style={{ width: 'auto' }} nowrap='true'>Số điện thoại</th>
-                    <th style={{ width: 'auto' }} nowrap='true'>Lý thuyết</th>
+                    {subjectColumns}
                     <th style={{ width: 'auto' }} nowrap='true'>Thực hành</th>
                     <th style={{ width: 'auto' }} nowrap='true'>Thi tốt nghiệp</th>
+                    <th style={{ width: 'auto' }} nowrap='true'>Thi sát hạch</th>
                 </tr>),
             renderRow: (item, index) => (
                 <tr key={index}>
@@ -76,9 +64,10 @@ class LecturerStudentPage extends AdminPage {
                     <TableCell type='text' content={item.lastname + ' ' + item.firstname} />
                     <TableCell type='text' content={item.identityCard} />
                     <TableCell type='text' content={item.user ? item.user.phoneNumber : ''} />
-                    <TableCell type='text' style={{ textAlign: 'center' }} content={this.checkMonLyThuyet(item,subjects) ? 'Đạt' : 'X'} />
-                    <TableCell type='text' style={{ textAlign: 'center' }} content={(item.diemThucHanh && item.diemThucHanh >=5) ? 'Đạt' : 'X'} />
+                    {monLyThuyet && monLyThuyet.length && monLyThuyet.map((subject, i) => (<TableCell key={i} type='text' style={{ textAlign: 'center' }} content={this.checkMonLyThuyet(item, subject) ? 'Đạt' : 'X'} />))}
+                    <TableCell type='text' style={{ textAlign: 'center' }} content={(item.diemThucHanh && item.diemThucHanh >= 5) ? 'Đạt' : 'X'} />
                     <TableCell type='text' style={{ textAlign: 'center' }} content={item.totNghiep ? 'Đạt' : 'X'} />
+                    <TableCell type='text' style={{ textAlign: 'center' }} content={item.datSatHach ? 'Đạt' : 'X'} />
                 </tr>),
         });
 
