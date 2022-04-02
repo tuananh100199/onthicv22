0b23213f219655ad0616ae35ab0778e092fb0d05
le vy <levy310898@gmail.com>
2022-04-02 10:52:48 +0700

    fix modal gplx

--- modules/_default/fwHome/sectionCacHangGplxModal.jsx
+++ modules/_default/fwHome/sectionCacHangGplxModal.jsx
@@ -12,9 +12,9 @@ export default class GplxModal extends React.Component {
     render() {
         let gplx = this.state.gplx;
         return (
-            <div ref={e => this.modal = e} className='modal fade bd-example-modal-lg' tabIndex='-1' role='dialog' aria-hidden='true'style={{width:'100vw',zIndex:1200}} >
+            <div ref={e => this.modal = e} className='gplx_modal modal fade bd-example-modal-lg' tabIndex='-1' role='dialog' aria-hidden='true'style={{width:'100vw',zIndex:1200}} >
                 <div className='modal-dialog modal-lg modal-dialog-centered'>
-                    <div className='modal-content' style={{border:'none',maxHeight:'60vh',overflowY:'auto'}}>
+                    <div className='modal-content' style={{border:'none'}}>
                     <div className='advisory_form_container' style={{backgroundColor:'#199D76'}}>
                         <h4 className='intro_form_title' style={{backgroundColor:'white',color:'#199D76'}}>{gplx.title||''}</h4>
                         {gplx.content && (gplx.content.split('\n') || []).map((subItem, index) => (
